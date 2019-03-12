32fd2f270199b808f7e28a3b43d3fc6693ce30b1
Amine Kharrou <amine.1017@gmail.com>
2019-03-12 20:03:15 +0100

    Implement navigation tabs opening logic

--- src/app/entry-point/entry.component.ts
+++ src/app/entry-point/entry.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import {filter, map, mergeMap} from 'rxjs/operators';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'risk-reveal-root',
   template:`<router-outlet></router-outlet>`
 })
 export class EntryComponent {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
+  }
 
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .pipe(map(() => this.activatedRoute))
+      .pipe(map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }))
+      .pipe(filter((route) => route.outlet === 'primary'))
+      .pipe(mergeMap((route) => route.data))
+      .subscribe((event) => this.titleService.setTitle(event['title']));
+  }
 }
--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {COMPONENTS} from "./components"
 import {CONTAINERS, WorkspaceMainComponent} from "./containers";
-import {RouterModule} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
 import {SharedModule} from "../shared/shared.module";
 
-const routes = [
-  {path:'',component:WorkspaceMainComponent}
+const routes:Routes = [
+  {data: {title: 'RR- Workspace'}, path:'',component: WorkspaceMainComponent},
+  {data: {title: 'RR- Workspace'}, path:':id',component: WorkspaceMainComponent},
 ]
 
 @NgModule({
