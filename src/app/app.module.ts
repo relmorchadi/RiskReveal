3e3fd58cfedfe59ed6bbea05e9fafaf6fce2cf98
Amine Kharrou <amine.1017@gmail.com>
2019-03-12 22:19:36 +0100

    Implement old search logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {GatewayModule} from './entry-point/gateway.module'
 import {EntryComponent} from './entry-point/entry.component';
 import {CoreModule} from "./core/core.module";
@@ -9,8 +8,6 @@ import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HelperService} from './shared/helper.service';
-
-
 
 
 @NgModule({
