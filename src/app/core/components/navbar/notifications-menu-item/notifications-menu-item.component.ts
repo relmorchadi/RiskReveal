eb2013f989575339ad0ddbaafc4f5c4dbc8deca4
Amine Kharrou <amine.1017@gmail.com>
2019-03-12 01:42:58 +0100

    Implement Menu navbar popovers

--- src/app/core/components/navbar/notifications-menu-item/notifications-menu-item.component.ts
+++ src/app/core/components/navbar/notifications-menu-item/notifications-menu-item.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-menu-item.component.scss']
 })
 export class NotificationsMenuItemComponent implements OnInit {
+  lastOnes= 5;
+  notification= {
+    all: {
+      today: [
+          {avatar: 'H.P', icon: null, iconColor: '#7ED321', content: 'Project P-6458 has been assigned to you by Huw Parry', backgroundColor: '#06B8FF', textColor: '#FFFFFF'},
+          {avatar: null,icon: 'icon-check_24px', iconColor: '#7ED321', content: 'Accumulation Package AP-3857 has been successfully published to ARC', backgroundColor: '#FCF9D6', textColor: 'rgba(0,0,0,0.6)'}
+      ],
+      yesterday: [
+        {avatar: null, icon: 'icon-check_circle_24px', iconColor: '#7ED321', content: 'Calculation on Inuring Package IP-2645 is now complete', backgroundColor: '#F4F6FC', textColor: 'rgba(0,0,0,0.6)'},
+        {avatar: null, icon: 'icon-poll_24px', iconColor: '#FFFFFF', content: 'Project P-8687 : <br/>7PLTs have been successfully published to pricing' , backgroundColor: '#0700CF', textColor: '#FFFFFF'}
+      ]
+    }
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleNotificaion(notification){
+
   }
 
 }
