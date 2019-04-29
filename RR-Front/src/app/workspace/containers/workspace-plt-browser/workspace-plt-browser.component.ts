1115164dc9e31642de1a95dbb7a8e843a7ec8605
elmehdi.achahboun <elmehdi.achahboun@berexia.ddns.net>
2019-04-29 18:53:29 +0200

    modifs 1.3

--- RR-Front/src/app/workspace/containers/workspace-plt-browser/workspace-plt-browser.component.ts
+++ RR-Front/src/app/workspace/containers/workspace-plt-browser/workspace-plt-browser.component.ts
@@ -32,26 +32,26 @@ export class WorkspacePltBrowserComponent implements OnInit {
     {pltId: 13 ,systemTags:[{tagId: 1}, {tagId: 2}, {tagId: 3}], userTags: [{tagId: 1}, {tagId: 2}], pltName:"NATC-USM_RL_Imf.T1", peril:"TC" , regionPerilCode:"NATC-USM", regionPerilName: "North Atlantic", selected: false, grain:"liberty-NAHU", vendorSystem:"RMS RiskLink", rap:"North Atlantic", d: true, note: true, checked: false}
   ];T
   systemTags = [
-    {tagId:'1', tagName:'TC', border: '#FFF', tagColor:'#7bbe31', innerTagContent:'1', innerTagColor:'#a2d16f'},
-    {tagId:'2', tagName:'NATC-USM', border: '#FFF', tagColor:'#7bbe31', innerTagContent:'2', innerTagColor:'#a2d16f'},
-    {tagId:'3', tagName:'Post-Inured', border: '#FFF', tagColor:'#006249', innerTagContent:'9', innerTagColor:'#4d917f'},
-    {tagId:'4', tagName:'Pricing', border: '#FFF', tagColor:'#009575', innerTagContent:'0', innerTagColor:'#4db59e'},
-    {tagId:'5', tagName:'Accumulation', border: '#FFF', tagColor:'#009575', innerTagContent:'2', innerTagColor:'#4db59e'},
-    {tagId:'6', tagName:'Default', border: '#FFF', tagColor:'#06b8ff', innerTagContent:'1', innerTagColor:'#51cdff'},
-    {tagId:'7', tagName:'Non-Default', border: '#FFF', tagColor:'#f5a623', innerTagContent:'0', innerTagColor:'#f8c065'},
+    {tagId:'1', tagName:'TC',  tagColor:'#7bbe31', innerTagContent:'1', innerTagColor:'#a2d16f', selected: false},
+    {tagId:'2', tagName:'NATC-USM',  tagColor:'#7bbe31', innerTagContent:'2', innerTagColor:'#a2d16f', selected: false},
+    {tagId:'3', tagName:'Post-Inured',  tagColor:'#006249', innerTagContent:'9', innerTagColor:'#4d917f', selected: false},
+    {tagId:'4', tagName:'Pricing',  tagColor:'#009575', innerTagContent:'0', innerTagColor:'#4db59e', selected: false},
+    {tagId:'5', tagName:'Accumulation',  tagColor:'#009575', innerTagContent:'2', innerTagColor:'#4db59e', selected: false},
+    {tagId:'6', tagName:'Default',  tagColor:'#06b8ff', innerTagContent:'1', innerTagColor:'#51cdff', selected: false},
+    {tagId:'7', tagName:'Non-Default',  tagColor:'#f5a623', innerTagContent:'0', innerTagColor:'#f8c065', selected: false},
   ];
   userTags = [
-    {tagId:'1', tagName:'Pricing V1', border: '#FFF', tagColor:'#893eff', innerTagContent:'1', innerTagColor:'#ac78ff'},
-    {tagId:'2', tagName:'Pricing V2', border:'#666666', tagColor:'#06b8ff', innerTagContent:'2', innerTagColor:'#51cdff'},
-    {tagId:'3', tagName:'Final Princing', border: '#FFF', tagColor:'#c38fff', innerTagContent:'5', innerTagColor:'#d5b0ff'}
+    {tagId:'1', tagName:'Pricing V1',  tagColor:'#893eff', innerTagContent:'1', innerTagColor:'#ac78ff'},
+    {tagId:'2', tagName:'Pricing V2',  tagColor:'#06b8ff', innerTagContent:'2', innerTagColor:'#51cdff'},
+    {tagId:'3', tagName:'Final Princing',  tagColor:'#c38fff', innerTagContent:'5', innerTagColor:'#d5b0ff'}
   ];
 
 paths = [
-  {id: '1', icon: 'icon-assignment_24px', text: 'P-1686 Ameriprise 2018'},
-  {id: '2', icon: 'icon-assignment_24px', text: 'P-1724 Trinity Kemper 2018'},
-  {id: '3', icon: 'icon-assignment_24px', text: 'P-8592 TWIA 2018'},
-  {id: '4', icon: 'icon-assignment_24px', text: 'P-9035 Post-insured PLTs'},
-  {id: '5', icon: 'icon-sitemap', text: 'IP-1135 CXL2 Cascading 2018'}
+  {id: '1', icon: 'icon-assignment_24px', text: 'P-1686 Ameriprise 2018', selected: false},
+  {id: '2', icon: 'icon-assignment_24px', text: 'P-1724 Trinity Kemper 2018', selected: false},
+  {id: '3', icon: 'icon-assignment_24px', text: 'P-8592 TWIA 2018', selected: false},
+  {id: '4', icon: 'icon-assignment_24px', text: 'P-9035 Post-insured PLTs', selected: false},
+  {id: '5', icon: 'icon-sitemap', text: 'IP-1135 CXL2 Cascading 2018', selected: false}
 ];
 
 currencies = [
@@ -88,8 +88,19 @@ metrics = [
 ];
 
 theads = [
-  {title: 'base', cards: [
-      {id: 'ID: 222881'}
+  {title: 'Base', cards: [
+      {chip: 'ID: 222881', content: 'HDIGlobal_CC_IT1607_XCV_SV_SURPLUS_729'},
+      {chip: '1.25', content: 'Portfolio Evolution'}
+    ]},
+  {title: 'Default', cards: [
+      {chip: 'Event', content: 'Tsunami'}
+    ]},
+  {title: 'Analyst', cards: [
+      {chip: '1.13', content: 'ALAE'}
+    ]},
+  {title: 'Client', cards: [
+      {chip: '0.95', content: 'Cedant QI'},
+      {chip: 'ID: 232896', content: 'JEPQ_RL_DefAdj_CC_IT1607_GGDHHT7766'}
     ]}
 ]
 
@@ -127,4 +138,13 @@ theads = [
       return item[0].tagColor;
     return null
   }
+
+  selectPath(path){
+  this.paths.forEach(pa => {
+    pa.selected = false;
+  })
+    path.selected = true;
+  }
 }
+
+
