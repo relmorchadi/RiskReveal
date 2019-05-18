27438be4181523c05b2b0b3d9395bb8ee825769f
amine.kharrou <amine.kharrou@berexia.com>
2019-05-18 10:55:05 +0000

    Feat(?): Segregate Front/Back deployment

--- RR-Backend/src/main/java/com/scor/rr/RiskRevealApplication.java
+++ RR-Backend/src/main/java/com/scor/rr/RiskRevealApplication.java
@@ -2,9 +2,10 @@ package com.scor.rr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RiskRevealApplication {
+public class RiskRevealApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RiskRevealApplication.class, args);
