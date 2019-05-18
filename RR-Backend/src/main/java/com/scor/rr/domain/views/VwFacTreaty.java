944f3a38819fcd4a0482d86d4059d4220972f5bb
elbabemohamad <mohamad.elbabe@berexia.com>
2019-05-18 15:58:02 +0000

    Feat(?): Fac Treaty API

---
+++ RR-Backend/src/main/java/com/scor/rr/domain/views/VwFacTreaty.java
@@ -0,0 +1,39 @@
+package com.scor.rr.domain.views;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "VW_FAC_TREATY", schema = "dbo", catalog = "RR")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class VwFacTreaty {
+
+  @Id
+  private String id;
+  private String analysisName;
+  private String analysisCtrBusinessType;
+  private String analysisCtrId;
+  private Boolean analysisCtrEndorsementNmber;
+  private String analysisCtrFacNumber;
+  private String analysisCtrInsured;
+  private String analysisCtrLabel;
+  private Long analysisCtrLob;
+  private Boolean analysisCtrOrderNumber;
+  private Long analysisCtrSubsidiary;
+  private Integer analysisCtrYear;
+  private String assignDate;
+  private String assignedTo;
+  private String lastUpdateDate;
+  private String lastUpdatedBy;
+  private String modellingSystemInstance;
+
+}
---
+++ RR-Backend/src/main/java/com/scor/rr/repository/specification/BaseSpecification.java
@@ -0,0 +1,143 @@
+package com.scor.rr.repository.specification;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Predicate;
+import javax.persistence.metamodel.SingularAttribute;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class BaseSpecification<T, U> {
+
+    private final String wildcard = "%";
+
+    public abstract Specification<T> getFilter(U request);
+
+    private String containsLowerCase(String searchField) {
+        return  wildcard + searchField.toLowerCase() + wildcard;
+    }
+    protected String equals(String searchField) {
+        return  searchField.toLowerCase();
+    }
+
+    protected String betweenDate(String searchField) {
+        return  searchField.toLowerCase();
+    }
+
+    protected Specification<T> AttributeIn(SingularAttribute attribute, List<String> val) {
+        return (root, query, cb) -> {
+            if(val == null) return null;
+            // if(val.isEmpty()) return cb.isNull(root.get(attribute));
+            if(val.size()==0)  return null;
+            return cb.lower(root.get(attribute)).in(val.stream().map(String::toLowerCase).collect(Collectors.toList()));
+        };
+    }
+
+    protected Specification<T> AttributeEquals(SingularAttribute attribute, String value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.like(cb.lower(root.get(attribute)), containsLowerCase(value));
+        };
+    }
+
+    protected Specification<T> AttributeGreaterToEqual(SingularAttribute attribute, Double value){
+        return (root, query, cb) -> {
+            if(value == null) {
+                return null;
+            }
+            return cb.greaterThanOrEqualTo(root.get(attribute),value );
+
+        };
+    }
+    protected Specification<T> AttributeGreaterToEqual(SingularAttribute attribute, Integer value){
+        return (root, query, cb) -> {
+            if(value == null) {
+                return null;
+            }
+            return cb.greaterThanOrEqualTo(root.get(attribute),value );
+
+        };
+    }
+    protected Specification<T> AttributeEquals(SingularAttribute attribute, Integer value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.equal(root.get(attribute), value);
+        };
+    }
+    protected Specification<T> AttributeEquals(SingularAttribute attribute, Object value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.equal(root.get(attribute), value);
+        };
+    }
+    protected Specification<T> AttributeContains(SingularAttribute attribute, String value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.like(cb.lower(root.get(attribute)),containsLowerCase(value));
+        };
+    }
+
+    protected Specification<T> AttributeEquals(SingularAttribute attribute, Double value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.equal(cb.function("ceiling", Integer.class, root.get(attribute)), value);
+        };
+    }
+    protected Specification<T> AttributeEquals(SingularAttribute attribute, Date value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(value);
+            int year=calendar.get(Calendar.YEAR);
+            int month=calendar.get(Calendar.MONTH);
+            int day=calendar.get(Calendar.DAY_OF_MONTH);
+            Predicate y = cb.equal(cb.function("year", Integer.class, root.get(attribute)), year);
+            Predicate m = cb.equal(cb.function("month", Integer.class, root.get(attribute)), month+1); // month commence par 0
+            Predicate d = cb.equal(cb.function("day", Integer.class, root.get(attribute)), day);
+            return cb.and(y,m,d);
+        };
+    }
+
+    protected Specification<T> dateGreaterThanOrEqualTo(SingularAttribute attribute, Date value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.greaterThanOrEqualTo(root.get(attribute), value);
+        };
+    }
+
+    protected Specification<T> dateLessThanOrEqualTo(SingularAttribute attribute, Date value) {
+        return (root, query, cb) -> {
+            if(value == null) { return null; }
+            return cb.lessThanOrEqualTo(root.get(attribute), value);
+        };
+    }
+
+    protected Specification<T> AttributeContainsBoolean(SingularAttribute attribute, String value) {
+        return (root, query, cb) -> {
+            Boolean b = BooleanUtils.toBooleanObject(value);
+            if(b == null) { return null; }
+            return cb.equal(root.get(attribute), b);
+        };
+    }
+
+    protected Specification<T> AttributeContainsLong(SingularAttribute attribute, String value) {
+        return (root, query, cb) -> {
+            Long l;
+            try{ l= Long.parseLong(value);
+            }catch (Exception e){ return null; }
+            return cb.equal(root.get(attribute), l);
+        };
+    }
+
+    protected Specification<T> AttributeContainsInteger(SingularAttribute attribute, String value) {
+        return (root, query, cb) -> {
+            Integer year;
+            try{ year=Integer.parseInt(value);
+            }catch (Exception e){ return null; }
+            return cb.equal(root.get(attribute), year);
+        };
+    }
+}
