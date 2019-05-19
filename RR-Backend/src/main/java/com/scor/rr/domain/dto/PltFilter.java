450f6897e3a5ffdb1485df5552f27595bb867bac
amine.kharrou <amine.kharrou@berexia.com>
2019-05-19 12:11:47 +0000

    Impl(?): PLT Browser backend services

---
+++ RR-Backend/src/main/java/com/scor/rr/domain/dto/PltFilter.java
@@ -0,0 +1,84 @@
+package com.scor.rr.domain.dto;
+
+
+import org.springframework.util.StringUtils;
+
+public class PltFilter {
+
+    private String pltId;
+    private String pltName;
+    private String peril;
+    private String regionPerilCode;
+    private String regionPerilName;
+    private String grain;
+    private String vendorSystem;
+    private String rap;
+
+    public PltFilter() {
+    }
+
+    public String getPltId() {
+        return pltId;
+    }
+
+    public void setPltId(String pltId) {
+        this.pltId = StringUtils.isEmpty(pltId) ? null: pltId;
+    }
+
+    public String getPltName() {
+        return pltName;
+    }
+
+    public void setPltName(String pltName) {
+        this.pltName = StringUtils.isEmpty(pltName) ? null: pltName;
+    }
+
+    public String getPeril() {
+        return peril;
+    }
+
+    public void setPeril(String peril) {
+        this.peril = StringUtils.isEmpty(peril) ? null: peril;
+    }
+
+    public String getRegionPerilCode() {
+        return regionPerilCode;
+    }
+
+    public void setRegionPerilCode(String regionPerilCode) {
+        this.regionPerilCode = StringUtils.isEmpty(regionPerilCode) ? null: regionPerilCode;
+    }
+
+    public String getRegionPerilName() {
+        return regionPerilName;
+    }
+
+    public void setRegionPerilName(String regionPerilName) {
+        this.regionPerilName = StringUtils.isEmpty(regionPerilName) ? null: regionPerilName;
+    }
+
+    public String getGrain() {
+        return grain;
+    }
+
+    public void setGrain(String grain) {
+        this.grain = StringUtils.isEmpty(grain) ? null: grain;
+    }
+
+    public String getVendorSystem() {
+        return vendorSystem;
+    }
+
+    public void setVendorSystem(String vendorSystem) {
+        this.vendorSystem = StringUtils.isEmpty(vendorSystem) ? null: vendorSystem;
+    }
+
+    public String getRap() {
+        return rap;
+    }
+
+    public void setRap(String rap) {
+        this.rap = StringUtils.isEmpty(rap) ? null: rap;
+    }
+
+}
