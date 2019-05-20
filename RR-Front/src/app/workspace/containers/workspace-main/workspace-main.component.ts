53d317c1650f183ae08d5b8685bf444f6f5b7780
othmane12345 <34275484+othmane12345@users.noreply.github.com>
2019-05-20 23:07:59 +0000

    fix(?): fixed the search dynamique

--- RR-Front/src/app/workspace/containers/workspace-main/workspace-main.component.ts
+++ RR-Front/src/app/workspace/containers/workspace-main/workspace-main.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {HelperService} from '../../../shared/helper.service';
 import {SearchService} from '../../../core/service/search.service';
 import * as _ from 'lodash';
@@ -32,7 +32,7 @@ export class WorkspaceMainComponent implements OnInit {
   state$: Observable<WorkspaceMain>;
   state: WorkspaceMain = null;
 
-  constructor(private _helper: HelperService, private route: ActivatedRoute, private _searchService: SearchService, private store: Store, private _router: Router) {
+  constructor(private _helper: HelperService, private cdRef: ChangeDetectorRef, private route: ActivatedRoute, private _searchService: SearchService, private store: Store, private _router: Router) {
 
   }
 
@@ -43,6 +43,7 @@ export class WorkspaceMainComponent implements OnInit {
       ({wsId, year}: any) => {
         this.getSearchedWorkspaces(wsId, year);
       });
+    this.store.select(dt => dt).subscribe()
   }
 
   getSearchedWorkspaces(wsId = null, year = null) {
