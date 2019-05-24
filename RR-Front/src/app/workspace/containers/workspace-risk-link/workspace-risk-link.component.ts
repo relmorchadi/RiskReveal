1a82c0e683cfb28a1f47df15367daa3f17f6e8aa
othmane12345 <othmane.lahlou16@gmail.com>
2019-05-24 10:37:36 +0000

    Fix(?): risk Link scroller update

--- RR-Front/src/app/workspace/containers/workspace-risk-link/workspace-risk-link.component.ts
+++ RR-Front/src/app/workspace/containers/workspace-risk-link/workspace-risk-link.component.ts
@@ -377,6 +377,7 @@ export class WorkspaceRiskLinkComponent implements OnInit, OnDestroy {
   }
 
   onInputSearch(event) {
+    console.log('the searched Item is:', event.target.value);
     if (event.target.value.length > 2) {
       this.store.dispatch(new SearchRiskLinkEDMAndRDMAction({keyword: event.target.value, size: '20'}));
     } else {
@@ -385,9 +386,17 @@ export class WorkspaceRiskLinkComponent implements OnInit, OnDestroy {
   }
 
   loadItemsLazy(event) {
-    const listvalue = this.state.listEdmRdm.dataLength + 20;
-    this.store.dispatch(new SearchRiskLinkEDMAndRDMAction({keyword: this.state.listEdmRdm.searchValue, size: listvalue.toString()}))
-    this.detectChanges();
+    let sizePage = '';
+    if (event.first + event.rows > this.state.listEdmRdm.totalNumberElement) {
+      sizePage  = this.state.listEdmRdm.totalNumberElement.toString();
+    } else {
+      sizePage = event.first === 0 ? '20' : (event.first + event.rows).toString();
+    }
+    if (this.state.listEdmRdm.dataLength < event.first + event.rows) {
+      console.log('you called for :' + sizePage);
+      this.store.dispatch(new SearchRiskLinkEDMAndRDMAction({keyword: this.state.listEdmRdm.searchValue, size: sizePage}));
+    }
+
   }
 
   changeCollapse(value) {
