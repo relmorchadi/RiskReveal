4676ce3d3b6701420116d9fc994fad0366e661fa
reda elmorchadi <reda.elmorchadi@berexia.com>
2019-07-01 13:43:09 +0000

    Fix - [Calibration] adjustment-pop-up added

---
+++ RR-Front/src/app/workspace/components/calibration/adjustment-pop-up/adjustment-pop-up.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-adjustment-pop-up',
+  templateUrl: './adjustment-pop-up.component.html',
+  styleUrls: ['./adjustment-pop-up.component.scss']
+})
+export class AdjustmentPopUpComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
