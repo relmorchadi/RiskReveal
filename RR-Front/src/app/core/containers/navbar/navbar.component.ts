4c573d1996c76c676f04c224f817c0602274ed1b
amine.kharrou <amine.kharrou@berexia.com>
2019-07-06 11:37:03 +0100

    Quick Fix(?): Close PopIns on header navigation

--- RR-Front/src/app/core/containers/navbar/navbar.component.ts
+++ RR-Front/src/app/core/containers/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import {fromEvent} from 'rxjs';
 import {debounceTime} from 'rxjs/operators';
 import {Select, Store} from '@ngxs/store';
 import {SearchNavBarState} from "../../store/states";
+import {HelperService} from "../../../shared/helper.service";
 
 @Component({
   selector: 'app-navbar',
@@ -16,8 +17,9 @@ export class NavbarComponent implements OnInit {
 
   @Select(SearchNavBarState)
   searchState$;
+  componentName: string = 'global-nav-bar';
 
-  constructor(private store: Store) {
+  constructor() {
   }
 
   collapseLeftNavbar() {
@@ -29,11 +31,16 @@ export class NavbarComponent implements OnInit {
       .pipe(debounceTime(200))
       .subscribe(({target: {innerWidth}}: any) => {
         this.expandMenu = this.expandMenu ? !(innerWidth > 768) : this.expandMenu;
-    });
+      });
   }
+
   wiki() {
+    this.closeOpenedPopins();
     window.open('http://dcvprdwiki/rrwiki/index.php/Main_Page', '_blank');
   }
 
+  closeOpenedPopins() {
+    HelperService.headerBarPopinChange$.next({from: this.componentName});
+  }
 
 }
