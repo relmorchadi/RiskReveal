bf8c8de91a4c4a4a264adde1602e38caec0b557a
amine.kharrou <amine.kharrou@berexia.com>
2019-08-11 22:41:16 +0100

    Impl(?) Import file base mocked API

--- RR-Backend/src/main/java/com/scor/rr/rest/FileBaseImportResource.java
+++ RR-Backend/src/main/java/com/scor/rr/rest/FileBaseImportResource.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+import static java.util.Arrays.asList;
+
 @RestController
 @RequestMapping("api/filebaseimport")
 public class FileBaseImportResource {
@@ -23,9 +25,20 @@ public class FileBaseImportResource {
 
     @GetMapping("files-list")
     ResponseEntity<?> getFilesList(@RequestParam String path) {
-        return ResponseEntity.ok(
-                this.fileBaseImportService.listFilesFromSandbox(path)
-        );
+        try {
+            return ResponseEntity.ok(
+                    this.fileBaseImportService.listFilesFromSandbox(path)
+            );
+        } catch (Exception e) {
+            return ResponseEntity.ok(
+                    asList(
+                            "52_SAGI_XOL_2019 - SAGI_XOL_2019_EventSetId_7511_100k-1547193581-Original-20190111_155940.txt",
+                            "6_IDFL_all_cresta - Analysis_EventSetId_7511_100k-1547173370-Original-20190111_102249.txt",
+                            "5_IDFL_Cresta3 - Analysis_EventSetId_7511_100k-1547173150-Original-20190111_101910.txt",
+                            "21_Baoviet_XOL - Analysis_EventSetId_7511_100k-1548815065-Original-20190130_102425.txt"
+                    )
+            );
+        }
     }
 
     @GetMapping("folders-list")
