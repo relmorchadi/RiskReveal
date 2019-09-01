5bc2938e34833b14b17facd967ea379f11bfb97f
amine.kharrou <amine.kharrou@berexia.com>
2019-09-01 17:19:29 +0100

    Feat(?) Implement Inuring Graph Component with the JsPlumb Nodes manipulation capabilities

--- RR-Front/src/app/workspace/components/inuring/inuring-graph/inuring-graph.component.ts
+++ RR-Front/src/app/workspace/components/inuring/inuring-graph/inuring-graph.component.ts
@@ -1,79 +1,263 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {jsPlumbService} from "jsplumbtoolkit-angular";
-import {jsPlumbToolkit, jsPlumbUtil} from "jsplumbtoolkit";
-import {SimpleNodeComponent} from "../simple-node/simple-node.component";
+import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Dialogs, Surface} from "jsplumbtoolkit";
+import {jsPlumbService, jsPlumbSurfaceComponent} from "jsplumbtoolkit-angular";
+import {FinalNodeComponent,InputNodeComponent} from "../nodes";
+import {ContractNodeComponent, JoinNodeComponent, NoteNodeComponent} from "../nodes";
+import {BaseContainer} from "../../../../shared/base";
+import {Router} from "@angular/router";
+import {Actions, ofActionDispatched, Store} from "@ngxs/store";
+import {AddInputNode, AddJoinNode} from "../../../store/actions";
 
 @Component({
   selector: 'inuring-graph',
   templateUrl: './inuring-graph.component.html',
   styleUrls: ['./inuring-graph.component.scss']
 })
-export class InuringGraphComponent implements OnInit {
+export class InuringGraphComponent extends BaseContainer implements OnInit, AfterViewInit, OnDestroy {
 
-  // @ViewChild(FlowchartComponent) flowchart:FlowchartComponent;
-  // @ViewChild(DatasetComponent) dataset:DatasetComponent;
+  surfaceId = 'flowchart';
+  toolkitId = 'flowchartSurface';
 
-  toolkitId:string;
-  toolkit:jsPlumbToolkit;
-  toolkitParams= {
-    nodeFactory: (type: string, data: any, callback: Function) => {
-      data.id = jsPlumbUtil.uuid();
-      callback(data);
-      // Dialogs.show({
-      //   id: 'dlgText',
-      //   title: 'Enter ' + type + ' name:',
-      //   onOK: (d: any) => {
-      //     data.text = d.text;
-      //     // if the user entered a name...
-      //     if (data.text) {
-      //       // and it was at least 2 chars
-      //       if (data.text.length >= 2) {
-      //         // set an id and continue.
-      //         data.id = jsPlumbUtil.uuid();
-      //         callback(data);
-      //       }
-      //       else
-      //       // else advise the user.
-      //         alert(type + ' names must be at least 2 characters!');
-      //     }
-      //     // else...do not proceed.
-      //   }
-      // });
+  surface: Surface;
+
+  view = {
+    nodes: {
+      'selectable': {
+        events: {
+          tap: (params: any) => {
+            if (params.node.data.ofType === 'stats') {
+              if (params.e.which === 3) return;
+              // this.store$.dispatch(new SelectLayoutToShowAction({id: 4, type: 'DEFAULT'}));
+              return;
+            }
+            // this.nodeName = params.node.data.name;
+            // this.toggleSelection(params.node);
+          },
+        }
+      },
+      'inputNode': {
+        parent: 'selectable',
+        component: InputNodeComponent,
+        events: {},
+      },
+      'contractNode': {
+        parent: 'selectable',
+        component: ContractNodeComponent,
+        events: {},
+      },
+      'joinNode': {
+        parent: 'selectable',
+        component: JoinNodeComponent,
+        events: {},
+      },
+      'finalNode': {
+        parent: 'selectable',
+        component: FinalNodeComponent,
+        events: {},
+      },
+      'noteNode': {
+        parent: 'selectable',
+        component: NoteNodeComponent,
+        events: {},
+      }
+
     },
-    beforeStartConnect: (node: any, edgeType: string) => {
-      return {label: ''};
+    groups: {
+      default: {
+        component: InputNodeComponent,
+        template: 'group',
+        endpoint: 'Blank',
+        anchor: 'Continuous',
+        revert: true,
+        orphan: true,
+        constrain: false,
+        autoSize: false,
+        droppable: true
+      },
+      constrained: {
+        parent: 'default',
+        constrain: true
+      }
+    },
+    edges: {
+      default: {
+        anchor: 'Continuous',
+        endpoint: 'Blank',
+        // connector: ['Flowchart', {cornerRadius: 5}],
+        paintStyle: {strokeWidth: 2, stroke: 'rgb(132, 172, 179)', outlineWidth: 3, outlineStroke: 'transparent'},
+        hoverPaintStyle: {strokeWidth: 2, stroke: '#b95124'}, // hover paint style for this edge type.
+        events: {
+          dblclick: (params: any) => {
+            Dialogs.show({
+              id: 'dlgConfirm',
+              data: {
+                msg: 'Delete Edge'
+              },
+              onOK: () => {
+                // this.removeEdge(params.edge);
+              }
+            });
+          }
+        },
+        overlays: [
+          ['Arrow', {location: 1, width: 10, length: 10}],
+          ['Arrow', {location: 0.3, width: 10, length: 10}]
+        ]
+      },
+      dashed: {
+        Anchor: 'Continuous',
+        Endpoint: 'Blank',
+        Connector: ['StateMachine', {
+          cssClass: 'connectorClass',
+          hoverClass: 'connectorHoverClass'
+        }],
+        PaintStyle: {strokeWidth: 4, stroke: '#89bcde'},
+        HoverPaintStyle: {stroke: '#4490de'},
+        events: {
+          dblclick: (params: any) => {
+            Dialogs.show({
+              id: 'dlgConfirm',
+              data: {
+                msg: 'Delete Edge'
+              },
+              onOK: () => {
+                // this.removeEdge(params.edge);
+              }
+            });
+          }
+        },
+        Overlays: [
+          [
+            'Label',
+            {
+              cssClass: 'delete-relationship',
+              id: function (params) {
+                console.log(params);
+              },
+              label: function (params) {
+                return '';
+              },
+
+              events: {}
+            }],
+          [
+            'Arrow', {
+            cssClass: 'arrow dashArrow',
+            fillStyle: '#445566',
+            width: 10,
+            length: 10,
+            location: 1
+          }]
+        ]
+      },
+    },
+    ports: {
+      start: {
+        endpoint: 'Blank',
+        anchor: 'Continuous',
+        uniqueEndpoint: true,
+        edgeType: 'default',
+        allowLoopback: false,
+        allowNodeLoopback: false,
+      },
+      source: {
+        endpoint: 'Blank',
+        paintStyle: {fill: '#84acb3'},
+        anchor: 'Continuous',
+        maxConnections: -1,
+        edgeType: 'connection',
+        allowLoopback: false,
+        allowNodeLoopback: false,
+      },
+      target: {
+        maxConnections: -1,
+        endpoint: 'Blank',
+        anchor: 'Continuous',
+        paintStyle: {fill: '#84acb3'},
+        isTarget: true,
+        allowLoopback: false,
+        allowNodeLoopback: false,
+      },
     }
   };
-  surfaceId='rr';
 
-  constructor(private $jsplumb:jsPlumbService, private elementRef:ElementRef) {
-    // this.toolkitId = this.elementRef.nativeElement.getAttribute("toolkitId");
-    this.toolkitId = 'flowchart';
+  renderParams = {
+    layout: {
+      type: 'Spring'
+    },
+    events: {
+      edgeAdded: (params: any) => {
+        if (params.addedByMouse) {
+          // this.editLabel(params.edge);
+        }
+      },
+      nodeDropped: (info) => {
+        console.log('node ', info.source.id, 'dropped on ', info.target.id);
+      }
+    },
+    consumeRightClick: false,
+    elementsDroppable: true,
+    dragOptions: {
+      start: (params) => {
+        console.log('drag Start', params);
+      },
+      stop: (params) => {
+        console.log('drag End', params);
+      },
+      filter: '.jtk-draw-handle, .node-action, .node-action i'
+    }
+  };
+
+  @ViewChild(jsPlumbSurfaceComponent)
+  surfaceComponent: jsPlumbSurfaceComponent;
+
+  toolkit;
+
+  constructor(private $jsplumb: jsPlumbService, private _router: Router, private _cdRef: ChangeDetectorRef, private _store: Store, private _actions: Actions) {
+    super(_router, _cdRef, _store);
   }
 
-  ngOnInit() {
-    this.toolkit = this.$jsplumb.getToolkit(this.toolkitId, this.toolkitParams)
-
-    this.toolkit.load({
-      data: {
-        nodes:[ {id:"1", name:"one"}, {id:"2", name:"two"}],
-        edges:[ {source:"1", target:"2" } ]
-      }
+  ngOnInit(): void {
+    this._actions.pipe(
+      this.unsubscribeOnDestroy,
+      ofActionDispatched(AddInputNode, AddJoinNode)
+    ).subscribe(action => {
+      console.log('Actions here')
+      if (action instanceof AddInputNode)
+        return this.toolkit.addNode({type: 'inputNode'});
+      else if (action instanceof AddJoinNode)
+        return this.toolkit.addNode({type: 'joinNode'});
     })
   }
 
-  view= {
-    nodes:{
-      default:{
-        component:SimpleNodeComponent
-      }
-    }
-  };
+  ngAfterViewInit(): void {
+    this.surface = window['surface'] = this.surfaceComponent.surface;
+    this.toolkit = window['toolkit'] = this.$jsplumb.getToolkit(this.toolkitId);
+    this.toolkit.addNode({type: 'inputNode'});
+    // this.toolkit.addNode({type: 'contractNode'});
+    this.toolkit.addNode({type: 'joinNode'});
+    this.toolkit.addNode({type: 'finalNode'});
+  }
 
-  renderParams= {
-    layout:{
-      type:"Spring"
-    }
-  };
+  drop(param) {
+    this.toolkit.addNode({type: 'contractNode'});
+  }
+
+  refreshToolkit() {
+    this.toolkit.clear();
+    this.toolkit.addNode({type: 'finalNode'});
+  }
+
+  addJoinNode() {
+    this.toolkit.addNode({type: 'joinNode'});
+  }
+
+  addNoteNode(){
+    this.toolkit.addNode({type: 'noteNode'});
+  }
+
+  ngOnDestroy(): void {
+    this.destroy();
+  }
 
 }
