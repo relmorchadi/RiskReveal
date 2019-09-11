422f3bcd4b1f18b1d52353812e8969b017f5e50e
amine.kharrou <amine.kharrou@berexia.com>
2019-09-11 09:02:37 +0100

    Feat(?) Impl detailed Analysis Scan

--- RR-Backend/src/main/java/com/scor/rr/rest/RiskLinkResource.java
+++ RR-Backend/src/main/java/com/scor/rr/rest/RiskLinkResource.java
@@ -5,6 +5,7 @@ import com.scor.rr.domain.EdmRdm;
 import com.scor.rr.domain.PortfolioView;
 import com.scor.rr.domain.dto.AnalysisFilter;
 import com.scor.rr.domain.dto.PortfolioFilter;
+import com.scor.rr.domain.views.AnalysisDetailView;
 import com.scor.rr.service.RiskLinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,7 +13,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/risk-link")
@@ -30,6 +34,13 @@ public class RiskLinkResource {
     public Page<AnalysisView> searchAnalysis(AnalysisFilter filter, @PageableDefault Pageable pageable){
         return riskLinkService.searchAnalysis(filter, pageable);
     }
+
+    @GetMapping("detailed-analysis-scan")
+    public List<AnalysisDetailView> searchAnalysis(@RequestParam Double analysisId, @RequestParam String analysisName){
+        return riskLinkService.analysisDetailsScan(analysisId, analysisName);
+    }
+
+
 
     @GetMapping("portfolio")
     public Page<PortfolioView> searchPortfolio(PortfolioFilter filter, @PageableDefault Pageable pageable){
