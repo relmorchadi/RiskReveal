595153c696fdf7b1f02afdc78872eade24d6791b
NGUYEN Viet Thanh Trung (EXTERNAL) <VNGUYEN-EXTERNAL@scor.com>
2019-09-13 14:42:57 +0200

    Inuring Edge enity + repository

---
+++ RR-Inuring/src/main/java/com/scor/rr/entity/InuringFinancialTreatment.java
@@ -0,0 +1,8 @@
+package com.scor.rr.entity;
+
+/**
+ * Created by u004602 on 13/09/2019.
+ */
+public enum InuringFinancialTreatment {
+    Positive, Negative;
+}
---
+++ RR-Inuring/src/main/java/com/scor/rr/repository/InuringEdgeRepository.java
@@ -0,0 +1,10 @@
+package com.scor.rr.repository;
+
+import com.scor.rr.entity.InuringEdge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by u004602 on 13/09/2019.
+ */
+public interface InuringEdgeRepository extends JpaRepository<InuringEdge, Integer> {
+}
