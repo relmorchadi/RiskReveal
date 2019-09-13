2be012ba90772deba761ed83cc706290a9e235bd
soufiane izend <soufiane.izend@berexia.com>
2019-09-13 17:19:16 +0200

    console.log

--- RR-Front/src/app/workspace/containers/workspace-scope-completence/workspace-scope-completence.component.ts
+++ RR-Front/src/app/workspace/containers/workspace-scope-completence/workspace-scope-completence.component.ts
@@ -359,6 +359,7 @@ export class WorkspaceScopeCompletenceComponent extends BaseContainer implements
         }
       )
     }
+    console.log("this is the listOfplts:",this.listOfPltsData);
 
     if (_.includes(holder, 'dispoWs')) {
       checked = 'dispoWs';
