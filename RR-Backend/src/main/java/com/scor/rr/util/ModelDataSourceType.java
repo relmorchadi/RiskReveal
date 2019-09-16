458d2f838b6be2bd0c1a713c02961a3d201320be
EU\U007632 <U007632>
2019-09-16 13:52:08 +0200

    Feat(?) Model DataSource Type Enum

---
+++ RR-Backend/src/main/java/com/scor/rr/util/ModelDataSourceType.java
@@ -0,0 +1,8 @@
+package com.scor.rr.util;
+
+class ModelDataSourceType {
+    final  static String RDM = "RDM";
+    final static String EDM = "EDM";
+    final static String Unknown = "Unknown";
+
+}
