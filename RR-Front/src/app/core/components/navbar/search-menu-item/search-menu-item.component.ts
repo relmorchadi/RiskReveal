2d72c7b22e5b8b8ce9852fc1f52754482a3464d9
reda elmorchadi <reda.elmorchadi@berexia.com>
2019-10-22 10:14:09 +0000

    [Feature] - Search - Implement the ability to apply one of the recent search on the expert mode + Edit Search Logic

--- RR-Front/src/app/core/components/navbar/search-menu-item/search-menu-item.component.ts
+++ RR-Front/src/app/core/components/navbar/search-menu-item/search-menu-item.component.ts
@@ -117,8 +117,10 @@ export class SearchMenuItemComponent implements OnInit, OnDestroy {
 
   onEnter(evt: KeyboardEvent) {
     evt.preventDefault();
+    console.log(this.isExpertMode);
     if (this.isExpertMode) {
-      this.store.dispatch(new SearchActions.ExpertModeSearchAction(this.globalKeyword));
+      console.log(this.convertBadgeToExpression(this.state.badges) + this.globalKeyword);
+      this.store.dispatch(new SearchActions.ExpertModeSearchAction(this.convertBadgeToExpression(this.state.badges) + this.globalKeyword));
     } else {
       this.store.dispatch(new SearchActions.SearchAction(this.state.badges, this.globalKeyword));
     }
@@ -132,6 +134,49 @@ export class SearchMenuItemComponent implements OnInit, OnDestroy {
       this.selectSearchBadge(HelperService.upperFirstWordsInSetence(this.state.tables[i]), this.state.data[i][j].label);
       this.scrollParams.scrollTo = -1;
     }
+  }
+
+  convertBadgeToExpression(badges) {
+    let globalExpression = "";
+    let expression;
+    _.forEach(badges, badge => {
+      switch (badge.key) {
+        case "Cedant Name":
+          expression = "c:" + badge.value;
+          break;
+        case "Cedant Code":
+          expression = "cid:" + badge.value;
+          break;
+        case "Country":
+          expression = "ctr:" + badge.value;
+          break;
+        case "Year":
+          expression = "uwy:" + badge.value;
+          break;
+        case "Workspace Name":
+          expression = "w:" + badge.value;
+          break;
+        case "Workspace Code":
+          expression = "wid:" + badge.value;
+          break;
+        case "Project":
+          expression = "p:" + badge.value;
+          break;
+        case "PLT":
+          expression = "plt:" + badge.value;
+          break;
+        case "Section Name":
+          expression = "s:" + badge.value;
+          break;
+        case "UW Unit":
+          expression = "uwu:" + badge.value;
+          break;
+        default:
+          expression = badge.value;
+      }
+      globalExpression += expression + " ";
+    })
+    return globalExpression;
   }
 
   onBackspace(evt: KeyboardEvent) {
@@ -232,9 +277,11 @@ export class SearchMenuItemComponent implements OnInit, OnDestroy {
   }
 
   onChangeTagValue(badge) {
-    let index = _.findIndex(this.state.badges, row => row.key == badge.key);
-    this.state.badges[index] = badge;
-    this.store.dispatch(new SearchActions.SearchAction(this.state.badges, this.globalKeyword));
+    if (this.isExpertMode) {
+      let index = _.findIndex(this.state.badges, row => row.key == badge.key);
+      this.store.dispatch(new SearchActions.CloseBadgeByIndexAction(index, this.isExpertMode));
+      this.contractFilterFormGroup.patchValue({globalKeyword: this.convertBadgeToExpression([badge])});
+    }
   }
 
   toggleInput(event: boolean, item) {
