2e5cf14c2569c1c5c3d4db80e9a33f24def98df5
NGUYEN Viet Thanh Trung (EXTERNAL) <VNGUYEN-EXTERNAL@scor.com>
2019-10-24 10:55:20 +0200

    typo while merging

--- RR-Common/src/main/java/com/scor/rr/exceptions/inuring/InuringContractLayerNotFoundException.java
+++ RR-Common/src/main/java/com/scor/rr/exceptions/inuring/InuringContractLayerNotFoundException.java
@@ -6,8 +6,8 @@ import com.scor.rr.exceptions.RRException;
 /**
  * Created by U004602 on 21/10/2019.
  */
-public class InuringContractLayerNotFound extends RRException {
-    public InuringContractLayerNotFound(int id) {
+public class InuringContractLayerNotFoundException extends RRException {
+    public InuringContractLayerNotFoundException(int id) {
         super(ExceptionCodename.INURING_CONTRACT_LAYER_NOT_FOUND, "Inuring Contract Layer id " + id + " not found");
     }
 }
