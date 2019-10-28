8d2c316ab2c38c105e87a511b8121b83d295e5e3
soufiane izend <soufiane.izend@berexia.com>
2019-10-28 09:06:09 +0100

    [Inuring] - FEATURE - Implementing some of the tests

--- RR-Common/src/main/java/com/scor/rr/exceptions/inuring/IllogicalEdgeCreationException.java
+++ RR-Common/src/main/java/com/scor/rr/exceptions/inuring/IllogicalEdgeCreationException.java
@@ -5,6 +5,6 @@ import com.scor.rr.exceptions.RRException;
 
 public class IllogicalEdgeCreationException extends RRException {
     public IllogicalEdgeCreationException(int id) {
-        super(ExceptionCodename.ILLOGICAL_EDGE_CREATION,"IllogicalEdgeCreation ");
+        super(ExceptionCodename.ILLOGICAL_EDGE_CREATION,"IllogicalEdgeCreation");
     }
 }
