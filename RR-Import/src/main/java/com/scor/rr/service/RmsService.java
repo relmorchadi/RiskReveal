ed862674f0af64bc3ef43cfdd1aa4e980a65fabd
Amine KHARROU <amine.kharrou@gmail.com>
2019-10-30 11:16:06 +0100

    Set Properties Default Schema to 'ImportBatch'

--- RR-Import/src/main/java/com/scor/rr/service/RmsService.java
+++ RR-Import/src/main/java/com/scor/rr/service/RmsService.java
@@ -49,9 +49,6 @@ public class RmsService {
     RlAnalysisRepository rlAnalysisRepository;
 
     @Autowired
-    RRFinancialPerspectiveRepository rrFinancialPerspectiveRepository;
-
-    @Autowired
     RlSourceResultRepository rlSourceResultRepository;
 
     @Autowired
