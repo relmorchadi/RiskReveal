4d3febb999de966e9a93920830cf7ac76979fd44
Najih Driss <najih.driss73@gmail.com>
2019-11-04 16:45:44 +0100

    Imp(?): Backend SORT

---
+++ RR-Backend/src/main/java/com/scor/rr/domain/enums/OrderType.java
@@ -0,0 +1,4 @@
+package com.scor.rr.util;
+
+public enum OrderType {
+}
--- RR-Backend/src/main/java/com/scor/rr/rest/SearchResource.java
+++ RR-Backend/src/main/java/com/scor/rr/rest/SearchResource.java
@@ -9,6 +9,7 @@ import com.scor.rr.service.TargetBuild.ShortCutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -81,4 +82,10 @@ public class SearchResource {
     List<ShortCut> getShortCuts() {
         return this.shortCutService.getShortCuts();
     }
+
+    @GetMapping("tesst")
+    public void test(@PageableDefault Pageable pageable){
+
+    }
+
 }
