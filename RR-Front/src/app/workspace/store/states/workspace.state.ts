f25037c70e7d5ebcd7813fbdf1d501213eca7aed
Najih Driss <najih.driss73@gmail.com>
2019-11-04 10:07:48 +0100

    Imp(?): merge with current master

--- RR-Front/src/app/workspace/store/states/workspace.state.ts
+++ RR-Front/src/app/workspace/store/states/workspace.state.ts
@@ -368,7 +368,7 @@ export class WorkspaceState {
 
   /***********************************
    *
-   * Scope And Completeness Selectors
+   * Scope And Completeness Selectors 
    *
    ***********************************/
 
