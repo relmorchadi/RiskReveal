0b6d1ce189d0267a69a455b8525e58767f89a5b8
Najih Driss <najih.driss73@gmail.com>
2019-11-07 17:47:24 +0100

    Imp(?): merge saved saerch branch & implement specific saved search to user (Treaty saved search completed)

--- RR-Backend/src/main/java/com/scor/rr/rest/SearchResource.java
+++ RR-Backend/src/main/java/com/scor/rr/rest/SearchResource.java
@@ -1,14 +1,17 @@
 package com.scor.rr.rest;
 
 import com.scor.rr.domain.*;
+import com.scor.rr.domain.TargetBuild.Search.ShortCut;
 import com.scor.rr.domain.dto.*;
 import com.scor.rr.domain.enums.SearchType;
 import com.scor.rr.domain.views.VwFacTreaty;
 import com.scor.rr.service.SearchService;
+import com.scor.rr.service.TargetBuild.ShortCutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +23,9 @@ public class SearchResource {
 
     @Autowired
     private SearchService searchService;
+
+    @Autowired
+    ShortCutService shortCutService;
 
     @GetMapping("treaty")
     Page<Treaty> searchTreaty(@RequestParam String keyword, int size) {
@@ -42,40 +48,38 @@ public class SearchResource {
     }
 
     @GetMapping("workspace")
-//    Page<ContractSearchResult> searchWorkspace(@RequestBody WorkspaceFilter filter, int size){
+//    Page<CATContract> searchWorkspace(@RequestBody WorkspaceFilter filter, int size){
     Page<?> globalSearchWorkspace(NewWorkspaceFilter filter, int offset, int size) {
         return searchService.globalSearchWorkspaces(filter, offset, size);
     }
 
 //    @PostMapping("workspace")
-//    Page<ContractSearchResult> searchWorkspace(@RequestBody WorkspaceFilter filter, int size){
+//    Page<CATContract> searchWorkspace(@RequestBody WorkspaceFilter filter, int size){
 //    Page<WorkspaceProjection> searchWorkspace(@RequestBody NewWorkspaceFilter filter, int offset, int size){
 //        return searchService.getWorkspaces(filter, offset,size);
 //    }
 
     @GetMapping("searchcount")
-    Page<?> countInWorkspace(@RequestParam TableNames table, @RequestParam String keyword, @RequestParam(defaultValue = "5") int size) {
+    SearchCountResult countInWorkspace(@RequestParam TableNames table, @RequestParam String keyword, @RequestParam(defaultValue = "5") int size ){
         return searchService.countInWorkspace(table, keyword, size);
     }
 
     @GetMapping("worspace/{workspaceId}/{uwy}")
-    ResponseEntity<WorkspaceDetailsDTO> getWorkspaceDetails(@PathVariable("workspaceId") String worspaceId, @PathVariable("uwy") String uwy) {
-        return searchService.getWorkspaceDetails(worspaceId, uwy)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    WorkspaceDetailsDTO getWorkspaceDetails(@PathVariable("workspaceId") String workspaceId, @PathVariable("uwy") String uwy){
+        return  searchService.getWorkspaceDetails(workspaceId, uwy);
     }
 
     @PostMapping
-    ResponseEntity<?> saveSearch(@RequestParam SearchType searchType, @RequestBody List<SearchItem> items) {
+    ResponseEntity<?> saveSearch(@RequestParam SearchType searchType, @RequestBody List<SearchItem> items, @RequestBody Integer userId) {
         return ResponseEntity.ok(
-                searchService.saveSearch(searchType, items)
+                searchService.saveSearch(searchType, items, userId)
         );
     }
 
     @GetMapping("saved-search")
-    ResponseEntity<?> getSavedSearch(@RequestParam SearchType searchType) {
+    ResponseEntity<?> getSavedSearch(@RequestParam SearchType searchType, @RequestParam Integer userId) {
         return ResponseEntity.ok(
-                searchService.getSavedSearches(searchType)
+                searchService.getSavedSearches(searchType, userId)
         );
     }
 
@@ -94,4 +98,15 @@ public class SearchResource {
     Page<?> expertModeSearch(@RequestBody ExpertModeFilterRequest request) {
         return searchService.expertModeSearch(request);
     }
+
+    @GetMapping("shortcuts")
+    List<ShortCut> getShortCuts() {
+        return this.shortCutService.getShortCuts();
+    }
+
+    @GetMapping("tesst")
+    public void test(@PageableDefault Pageable pageable){
+
+    }
+
 }
