a5f07fba86ac5e27ceec2aa8f0ad681c0c423329
Najih Driss <najih.driss73@gmail.com>
2019-11-12 14:10:05 +0100

    Imp(?): project statistics procedures et APIs + fav/rec/ass/pin count api

--- RR-Backend/src/main/java/com/scor/rr/repository/TargetBuild/WorkspacePoPin/AssignedWorkspaceViewRepository.java
+++ RR-Backend/src/main/java/com/scor/rr/repository/TargetBuild/WorkspacePoPin/AssignedWorkspaceViewRepository.java
@@ -4,7 +4,9 @@ import com.scor.rr.domain.TargetBuild.WorkspacePoPin.AssignedWorkspaceView;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +14,9 @@ public interface AssignedWorkspaceViewRepository extends JpaRepository<AssignedW
 
     @Query("from AssignedWorkspaceView aws where aws.userId = :userId order by aws.createDate desc")
     List<AssignedWorkspaceView> findAllByUserId(@Param("userId") Integer userId, Pageable page);
+
+    @Transactional
+    @Procedure(procedureName = "dr.COUNT_ASSIGNED_Workspace", outputParameterName = "count")
+    Integer getAssignedWSCount(@Param("userId") Integer userId);
+
 }
