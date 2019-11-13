29aabfe7c2ea5d1b8aa93c60afe98edd6cb4b584
Najih Driss <najih.driss73@gmail.com>
2019-11-13 17:28:42 +0100

    Fix(?): hide all screen not in TB

--- RR-Backend/src/main/java/com/scor/rr/domain/TargetBuild/AccumulationPackage.java
+++ RR-Backend/src/main/java/com/scor/rr/domain/TargetBuild/AccumulationPackage.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AdjustmentNode {
+public class AccumulationPackage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
