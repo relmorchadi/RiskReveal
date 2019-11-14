a5314bd469b29f116e56a6a4c2e4c3ed7ebc758c
Najih Driss <najih.driss73@gmail.com>
2019-11-14 12:27:32 +0100

    Imp(?): added project statistics repos & entities

---
+++ RR-Backend/src/main/java/com/scor/rr/repository/TargetBuild/Project/NumberOfPLTsPublishedForPricingRepository.java
@@ -0,0 +1,8 @@
+package com.scor.rr.repository.TargetBuild.Project;
+
+
+import com.scor.rr.domain.TargetBuild.Project.NumberOfPLTsPublishedForPricing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NumberOfPLTsPublishedForPricingRepository extends JpaRepository<NumberOfPLTsPublishedForPricing, Long> {
+}
