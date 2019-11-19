721cb86172560ab5a6fb165f59ba803c791adb59
LAHLOU Othmane (EXTERNAL) <OLAHLOU-EXTERNAL@scor.com>
2019-11-19 15:03:30 +0100

    Fix(?): synchronize Workspace Header Pop In

--- RR-Front/src/app/core/components/navbar/workspaces-menu-item/workspaces-menu-item.component.ts
+++ RR-Front/src/app/core/components/navbar/workspaces-menu-item/workspaces-menu-item.component.ts
@@ -17,6 +17,11 @@ import * as workspaceActions from '../../../../workspace/store/actions/workspace
 import {UpdateWsRouting} from '../../../../workspace/store/actions/workspace.actions';
 import {NotificationService} from '../../../../shared/notification.service';
 import {BaseContainer} from '../../../../shared/base';
+import {WorkspaceState} from "../../../../workspace/store/states";
+import {take} from "rxjs/operators";
+import {Navigate} from "@ngxs/router-plugin";
+import {promise} from "selenium-webdriver";
+import delayed = promise.delayed;
 
 @Component({
   selector: 'workspaces-menu-item',
@@ -56,12 +61,18 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
   @Select(HeaderState.getFavoriteWs)favoriteWs$;
   @Select(HeaderState.getPinnedWs)pinnedWs$;
   @Select(HeaderState.getStatusCountWs)countWs$;
+  @Select(WorkspaceState.getLastWorkspace) lastWorkspace$;
 
   countWs: any;
   recentWs: any;
   favoriteWs: any;
   pinnedWs: any;
   assignedWs: any;
+
+  loadRecent = false;
+  loadFavorite = false;
+  loadAssigned = false;
+  loadPinned = false;
 
   recentSearch: any;
   favoriteSearch: any;
@@ -82,20 +93,18 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
   recentPagination = 0;
 
   ngOnInit() {
+    this.store.dispatch([new fromHD.LoadRecentWorkspace({offset: 0, size: 10, userId: 1}),
+      new fromHD.LoadWsStatusCount()]);
     this.countWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => this.countWs = _.merge({}, value));
-    this.recentWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => this.recentWs =  value);
-    this.favoriteWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => this.favoriteWs =  value);
-    this.pinnedWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => this.pinnedWs = value);
-    this.assignedWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => this.assignedWs = value);
+    this.recentWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => {this.recentWs =  value; this.detectChanges(); });
+    this.favoriteWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => {this.favoriteWs =  value; this.detectChanges(); });
+    this.pinnedWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => {this.pinnedWs = value; this.detectChanges(); });
+    this.assignedWs$.pipe(this.unsubscribeOnDestroy).subscribe(value => {this.assignedWs = value; this.detectChanges(); });
 
     // this.recentWs$.subscribe(value => this.recent = _.merge([], value));
 
     this._searchService.infodropdown.subscribe(dt => {
       this.visible = this._searchService.getvisibleDropdown();
-      if (this.visible) {
-        this.store.dispatch([new fromHD.LoadRecentWorkspace({offset: 0, size: 10, userId: 1}),
-          new fromHD.LoadWsStatusCount()]);
-      }
       this.detectChanges();
     });
 
@@ -117,14 +126,38 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
   }
 
   loadTabData(event) {
-    if (event === 0) {
+    if (event === 0 && !this.loadRecent) {
       this.store.dispatch(new fromHD.LoadRecentWorkspace({offset: 0, size: 10, userId: 1}));
-    } else if (event === 1) {
+      this.loadRecent = true;
+    } else if (event === 1 && !this.loadFavorite) {
       this.store.dispatch(new fromHD.LoadFavoriteWorkspace({offset: 0, size: 10, userId: 1}));
-    } else if (event === 2) {
+      this.loadFavorite = true;
+    } else if (event === 2 && !this.loadAssigned) {
       this.store.dispatch(new fromHD.LoadAssignedWorkspace({offset: 0, size: 10, userId: 1}));
-    } else if (event === 3) {
+      this.loadAssigned = true;
+    } else if (event === 3 && !this.loadPinned) {
       this.store.dispatch(new fromHD.LoadPinnedWorkspace({offset: 0, size: 10, userId: 1}));
+      this.loadPinned = true;
+    }
+  }
+
+  pageableChange($event, scope) {
+    if (scope === 'recent') {
+      const size =  $event - this.recentPageable;
+      this.store.dispatch(new fromHD.LoadRecentWorkspace({offset: this.recentPageable,
+        size, userId: 1, option: 'append'}));
+    } else if (scope === 'favorite') {
+      const size =  $event - this.favoritePageable;
+      this.store.dispatch(new fromHD.LoadFavoriteWorkspace({offset: this.favoritePageable,
+        size, userId: 1, option: 'append'}));
+    } else if (scope === 'assigned') {
+      const size =  $event - this.assignedPageable;
+      this.store.dispatch(new fromHD.LoadAssignedWorkspace({offset: this.assignedPageable,
+        size, userId: 1, option: 'append'}));
+    } else if (scope === 'pinned') {
+      const size =  $event - this.pinnedPageable;
+      this.store.dispatch(new fromHD.LoadPinnedWorkspace({offset: this.pinnedPageable,
+        size, userId: 1, option: 'append'}));
     }
   }
 
@@ -132,28 +165,54 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
     this.contractFilterFormGroup.patchValue({cedant: search.target.value});
   }
 
-  toggleWorkspace(context: string, workspace, index) {
-/*    if ((window as any).event.ctrlKey) {
-/!*      this.store.dispatch(new fromHeader.ToggleWsSelection({context, index}));*!/
-      // workspace.selected = !workspace.selected;
-      this.lastSelectedIndex = index;
+  singleLineSelect(scope, workspace, value = null) {
+    if (scope === 'recent') {
+      this.dispatch(new fromHD.ToggleRecentWsSelection({item: workspace, selection: 'single', value}));
+    } else if (scope === 'favorite') {
+      this.dispatch(new fromHD.ToggleFavoriteWsSelection({item: workspace, selection: 'single', value}));
+    } else if (scope === 'assigned') {
+      this.dispatch(new fromHD.ToggleAssignedWsSelection({item: workspace, selection: 'single', value}));
+    } else if (scope === 'pinned') {
+      this.dispatch(new fromHD.TogglePinnedWsSelection({item: workspace, selection: 'single', value}));
+    }
+  }
+
+  toggleWorkspace(scope: string, workspace, index) {
+    if ((window as any).event.ctrlKey) {
+      setTimeout(() => {
+        this.singleLineSelect(scope, workspace, true);
+      }, 200);
     } else if ((window as any).event.shiftKey) {
-      event.preventDefault();
-      if (this.lastSelectedIndex || this.lastSelectedIndex === 0) {
-        const [from, to] = [Math.min(index, this.lastSelectedIndex), Math.max(index, this.lastSelectedIndex)];
-        this.store.dispatch(new fromHeader.SelectRange({context, from, to}));
-      } else {
-        this.lastSelectedIndex = index;
-        this.store.dispatch(new fromHeader.ChangeWsSelection({context, index, value: true}));
-        // workspace.selected = true;
-      }
+      /*      if (this.lastSelectedIndex || this.lastSelectedIndex === 0) {
+              const [from, to] = [Math.min(index, this.lastSelectedIndex), Math.max(index, this.lastSelectedIndex)];
+              this.store.dispatch(new fromHeader.SelectRange({context, from, to}));
+            } else {
+              this.lastSelectedIndex = index;
+              this.store.dispatch(new fromHeader.ChangeWsSelection({context, index, value: true}));
+              // workspace.selected = true;
+            }*/
     } else {
-      // this.recent.recentWs.forEach(res => res.selected = false);
-      this.lastSelectedIndex = index;
-      this.store.dispatch(new fromHeader.ApplySelectionToAll({context, value: false}));
-      this.store.dispatch(new fromHeader.ToggleWsSelection({context, index}));
-      // workspace.selected = !workspace.selected;
-    }*/
+      setTimeout(() => {
+        if (scope === 'recent') {
+          this.dispatch(new fromHD.ToggleRecentWsSelection({selection: 'all', value: false}));
+          this.dispatch(new fromHD.ToggleRecentWsSelection({item: workspace, selection: 'single', value: true}));
+        } else if (scope === 'favorite') {
+          this.dispatch(new fromHD.ToggleFavoriteWsSelection({selection: 'all', value: false}));
+          this.dispatch(new fromHD.ToggleFavoriteWsSelection({item: workspace, selection: 'single', value: true}));
+        } else if (scope === 'assigned') {
+          this.dispatch(new fromHD.ToggleAssignedWsSelection({selection: 'all', value: false}));
+          this.dispatch(new fromHD.ToggleAssignedWsSelection({item: workspace, selection: 'single', value: true}));
+        } else if (scope === 'pinned') {
+          this.dispatch(new fromHD.TogglePinnedWsSelection({selection: 'all', value: false}));
+          this.dispatch(new fromHD.TogglePinnedWsSelection({item: workspace, selection: 'single', value: true}));
+        }}, 200);
+    }
+  }
+
+  async openSingleWorkspace(ws) {
+    console.log('redirection', ws);
+    this.router.navigate([`/workspace/${ws.workspaceContextCode}/${ws.workspaceUwYear}/projects`]);
+    this.visible = false;
   }
 
   selectCheckboxChange(context, workspace, index) {
@@ -167,47 +226,37 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
     });
   }
 
-  openWorkspaces() {
-    const selectedItems = this.recent.filter(ws => ws.selected);
-
+  openWorkspaces(scope) {
+    let selectedItems = null;
+    if (scope === 'recent') {
+      selectedItems = this.recentWs.filter(ws => ws.selected);
+    } else if (scope === 'favorite') {
+      selectedItems = this.favoriteWs.filter(ws => ws.selected);
+    } else if (scope === 'assigned') {
+      selectedItems = this.assignedWs.filter(ws => ws.selected);
+    } else if (scope === 'pinned') {
+      selectedItems = this.pinnedWs.filter(ws => ws.selected);
+    }
     let workspaces = [];
     selectedItems.forEach(
       (ws) => {
-        this.searchData(ws.wsId, ws.uwYear).subscribe(
+        this.searchData(ws.workspaceContextCode, ws.workspaceUwYear).subscribe(
           (dt: any) => {
             const workspace = {
-              workSpaceId: ws.workSpaceId,
-              uwYear: ws.uwYear,
+              workSpaceId: ws.workspaceContextCode,
+              uwYear: ws.workspaceUwYear,
               selected: false,
               ...dt
             };
-            this.store.dispatch(new workspaceActions.OpenWS({wsId: ws.wsId, uwYear: ws.uwYear, route: 'projects', type: 'treaty'}));
+            this.store.dispatch(new workspaceActions.OpenWS({wsId: ws.workspaceContextCode,
+              uwYear: ws.workspaceUwYear, route: 'projects', type: 'treaty'}));
             workspaces = [workspace, ...workspaces];
             if (workspaces.length === selectedItems.length) {
               this.visible = false;
-              this.workspaces.forEach(wss => wss.selected = false);
             }
             this.detectChanges();
           }
         );
-      }
-    );
-  }
-
-  async openSingleWorkspaces(ws) {
-    this.searchData(ws.workSpaceId, ws.uwYear).subscribe(
-      (dt: any) => {
-        const workspace = {
-          workSpaceId: ws.workSpaceId,
-          uwYear: ws.uwYear,
-          selected: false,
-          ...dt
-        };
-
-        this.store.dispatch(new OpenNewWorkspacesAction([workspace]));
-        this.visible = false;
-        this.workspaces.forEach(wcs => wcs.selected = false);
-        this.router.navigate([`/workspace/${this.recent.openedWs.workSpaceId}/${this.recent.openedWs.uwYear}`]);
       }
     );
   }
@@ -221,7 +270,7 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
   }
 
   redirectWorkspace() {
-/*    this.lastWorkspace$
+    this.lastWorkspace$
       .pipe(take(1))
       .subscribe(data => {
         if (data) {
@@ -235,7 +284,7 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
             'There is no Opened Workspaces please try searching for some before!',
             'error', 'bottomRight', 4000);
         }
-      });*/
+      });
 
   }
 
@@ -248,26 +297,29 @@ export class WorkspacesMenuItemComponent extends BaseContainer implements OnInit
     }));
   }
 
-  toggle(workspace: any, type: string, selected) {
-    switch (type) {
-      case 'favorite':
-        this.store.dispatch(new PatchWorkspace({
-          key: 'selectedF',
-          value: !selected,
-          ws: workspace,
-        }));
-        break;
-      case 'pinged':
-        this.store.dispatch(new PatchWorkspace({
-          key: 'selectedP',
-          value: !selected,
-          ws: workspace,
-        }));
-        break;
-    }
-  }
+  /*  toggle(workspace: any, type: string, selected) {
+      switch (type) {
+        case 'favorite':
+          this.store.dispatch(new PatchWorkspace({
+            key: 'selectedF',
+            value: !selected,
+            ws: workspace,
+          }));
+          break;
+        case 'pinged':
+          this.store.dispatch(new PatchWorkspace({
+            key: 'selectedP',
+            value: !selected,
+            ws: workspace,
+          }));
+          break;
+      }
+    }*/
 
   togglePopup() {
+    this.store.dispatch([new fromHD.LoadRecentWorkspace({offset: 0, size: 10, userId: 1}),
+      new fromHD.LoadWsStatusCount()]);
+    this.loadPinned = false; this.loadFavorite = false; this.loadAssigned = false; this.loadRecent = true;
     HelperService.headerBarPopinChange$.next({from: this.componentName});
   }
 
