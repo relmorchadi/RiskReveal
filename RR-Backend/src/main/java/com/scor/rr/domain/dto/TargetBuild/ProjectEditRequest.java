9e2dc186e8882762d0b63daadaec0844af161a90
EU\U008925 <U008925>
2019-11-20 17:22:34 +0100

    Imp(): edit project backend

---
+++ RR-Backend/src/main/java/com/scor/rr/domain/dto/TargetBuild/ProjectEditRequest.java
@@ -0,0 +1,11 @@
+package com.scor.rr.domain.dto.TargetBuild;
+
+import lombok.Data;
+
+@Data
+public class ProjectEditRequest {
+    private Long projectId;
+    private String projectName;
+    private String projectDescription;
+    private String assignedTo;
+}
