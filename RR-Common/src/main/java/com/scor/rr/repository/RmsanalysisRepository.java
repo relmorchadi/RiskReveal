27cc08cbbc7b4826177ad448e7e1622708f78619
NGUYEN Viet Thanh Trung (EXTERNAL) <VNGUYEN-EXTERNAL@scor.com>
2019-11-22 16:40:26 +0100

    follow up the cleansing

--- RR-Common/src/main/java/com/scor/rr/repository/RmsanalysisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.scor.rr.repository;
-
-import com.scor.rr.domain.RmsAnalysisEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RmsanalysisRepository extends JpaRepository<RmsAnalysisEntity, String> {
-}
