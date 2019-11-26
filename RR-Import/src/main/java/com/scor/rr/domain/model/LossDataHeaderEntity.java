5f4fb9e91c0cace9a8034d6971feb1ba3f889407
U008922 <AIKAR-EXTERNAL@SCOR.COM>
2019-11-26 17:49:39 +0100

    [Common] - Fix - LossDataHeader

--- RR-Import/src/main/java/com/scor/rr/domain/model/LossDataHeaderEntity.java
+++ RR-Import/src/main/java/com/scor/rr/domain/model/LossDataHeaderEntity.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class LossDataHeaderEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "LossDataHeaderId")
     private Long lossDataHeaderId;
     @Column(name = "Entity")
