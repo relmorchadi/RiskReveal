78a8b84fb8dd5e38d9e34a0f91b4d7f129182701
U008922 <AIKAR-EXTERNAL@SCOR.COM>
2019-11-29 17:00:50 +0100

    [Common] - Fix - ContractSourceType

--- RR-Common/src/main/java/com/scor/rr/domain/ContractSourceTypeEntity.java
+++ RR-Common/src/main/java/com/scor/rr/domain/ContractSourceTypeEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Contract")
+@Table(name = "ContractSourceType")
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContractSourceTypeEntity {
