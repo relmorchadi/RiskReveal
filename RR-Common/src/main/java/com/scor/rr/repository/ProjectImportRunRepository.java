7a4317a6e8cb5cae4fbbf6e63a990b2c6d47607e
U008922 <AIKAR-EXTERNAL@SCOR.COM>
2019-12-11 17:58:48 +0100

    [Import] - Feature - Job parameters preparation

--- RR-Common/src/main/java/com/scor/rr/repository/ProjectImportRunRepository.java
+++ RR-Common/src/main/java/com/scor/rr/repository/ProjectImportRunRepository.java
@@ -2,12 +2,18 @@ package com.scor.rr.repository;
 
 import com.scor.rr.domain.ProjectImportRunEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProjectImportRunRepository extends JpaRepository<ProjectImportRunEntity, Integer> {
 
     List<ProjectImportRunEntity> findByProjectId(Long projectId);
+
+    @Query("FROM ProjectImportRunEntity WHERE projectId =:projectId ORDER BY startDate")
+    List<ProjectImportRunEntity> findByProjectIdOrderedByStartDate(@Param("projectId") Long projectId);
+
     ProjectImportRunEntity findByProjectIdAndRunId(Long projectId, Integer runId);
 }
 
