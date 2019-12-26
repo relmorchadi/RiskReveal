71d598f3cd133c4fdbeb5e2520e97eee89898e66
EU\U008925 <U008925>
2019-12-26 16:02:08 +0100

    Fix(?): fix calib port 2

--- RR-Front/src/environments/environment.prod.ts
+++ RR-Front/src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: true,
   API_URI:'http://dcvdevalmf1:8880/risk-reveal/api/',
   IMPORT_URI:'http://dcvdevalmf1:9080/risk-reveal-import/api/',
+  CALIBRATION_URI: 'http://dcvdevalmf1:8980/risk-reveal-adjustment/',
   hmr:false
 };
 
