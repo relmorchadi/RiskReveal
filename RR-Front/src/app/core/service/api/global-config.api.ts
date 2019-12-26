64b388eb2b33646b134126605046ffd2e6e9a577
U009912 <OLAHLOU-EXTERNAL@scor.com>
2019-12-26 09:32:30 +0100

    Fix(?): fix General Config Saving Feature

--- RR-Front/src/app/core/service/api/global-config.api.ts
+++ RR-Front/src/app/core/service/api/global-config.api.ts
@@ -6,13 +6,21 @@ import {backendUrl} from '../../../shared/api';
   providedIn: 'root'
 })
 export class GlobalConfigApi {
-  protected URL = `${backendUrl()}globalConfig/`;
+  protected URL = `${backendUrl()}user-preferences/`;
 
   constructor(private http: HttpClient) {
   }
 
   postGlobalConfig(data) {
     return this.http.post(`${this.URL}`, data);
+  }
+
+  getGlobalConfig(userId) {
+    return this.http.get(`${this.URL}`, {params: {userId}});
+  }
+
+  delGlobalConfig(userPreferenceId) {
+    return this.http.delete(`${this.URL}`, {params: {userPreferenceId}});
   }
 
 }
