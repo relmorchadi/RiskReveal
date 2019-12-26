08fa60aac596e5043fc0ddc99fa0a332e7557e2a
U009568 <ITAHIRI-EXTERNAL@scor.com>
2019-12-26 10:06:04 +0100

    filter plts table by status

--- RR-Front/src/app/workspace/components/calibration-new/calibration-new-table/calibration-new-table.component.ts
+++ RR-Front/src/app/workspace/components/calibration-new/calibration-new-table/calibration-new-table.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Message} from "../../../../shared/message";
-
+import {StatusFilter} from "../../../model/status-filter.model";
+declare  const _;
 @Component({
   selector: 'app-calibration-new-table',
   templateUrl: './calibration-new-table.component.html',
@@ -15,10 +16,44 @@ export class CalibrationNewTableComponent implements OnInit {
   @Input() mode: 'default' | 'grouped';
   @Input() columns: any[];
   @Input() rowKeys: any;
-
+  selectedplts: any = [];
+  statusFilter: StatusFilter;
+  selectedStatusFilter: any = {};
   constructor() { }
 
   ngOnInit() {
+    this.statusFilter = new StatusFilter();
+    /*setInterval(()=>{
+      console.log(this.statusFilter);
+    },3000)*/
   }
 
+  statusFlilerCheckbox($event: any, type: string) {
+    switch (type) {
+      case 'inProgress':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'inProgress': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['inProgress'])
+        break;
+      case 'new':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'new': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['new'])
+        break;
+      case 'valid':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'valid': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['valid'])
+        break;
+      case 'locked':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'locked': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['locked'])
+        break;
+      case 'requiresRegeneration':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'requiresRegeneration': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['requiresRegeneration'])
+        break;
+      case 'failed':
+        if (event.target['checked']) this.selectedStatusFilter = {...this.selectedStatusFilter, 'failed': true};
+        else this.selectedStatusFilter = _.omit(this.selectedStatusFilter, ['failed'])
+        break;
+    }
+  }
 }
