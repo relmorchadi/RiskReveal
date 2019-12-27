4494fbcb82b540bc40e19ef35ba0d5656d9741c7
EU\U008925 <U008925>
2019-12-27 11:24:31 +0100

    Imp(?): adding defaultAdj get

--- RR-Common/src/main/java/com/scor/rr/repository/DefaultAdjustmentRepository.java
+++ RR-Common/src/main/java/com/scor/rr/repository/DefaultAdjustmentRepository.java
@@ -2,8 +2,12 @@ package com.scor.rr.repository;
 
 import com.scor.rr.domain.DefaultAdjustmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DefaultAdjustmentRepository extends JpaRepository<DefaultAdjustmentEntity,Integer> {
 
@@ -12,4 +16,9 @@ public interface DefaultAdjustmentRepository extends JpaRepository<DefaultAdjust
             long marketChannel_marketChannelId,
             String engineType,
             int entity_entityId);
+
+    @Transactional
+    @Query(value = "exec dbonew.usp_GetDefaultAdjustmentsInScope @workspaceContextCode=:workspaceContextCode, @uwYear=:uwYear", nativeQuery = true)
+    List<Map<String, Object>> getDefaultAdjustmentsInScope(@Param("workspaceContextCode") String workspaceContextCode, @Param("uwYear") Integer uwYear);
+
 }
