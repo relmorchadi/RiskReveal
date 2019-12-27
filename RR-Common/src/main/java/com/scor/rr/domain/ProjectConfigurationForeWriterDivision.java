a692cbbc24c77be191941ce683d3afd4b5382911
NGUYEN Viet Thanh Trung (EXTERNAL) <VNGUYEN-EXTERNAL@scor.com>
2019-12-27 16:39:03 +0100

    add constructors

--- RR-Common/src/main/java/com/scor/rr/domain/ProjectConfigurationForeWriterDivision.java
+++ RR-Common/src/main/java/com/scor/rr/domain/ProjectConfigurationForeWriterDivision.java
@@ -15,6 +15,16 @@ import javax.persistence.*;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ProjectConfigurationForeWriterDivision {
 
+    public ProjectConfigurationForeWriterDivision(Integer entity, Long projectConfigurationForeWriterContractId, String divisionNumber, String principalDivision, String lineOfBusiness, String coverageType, String currency) {
+        this.entity = entity;
+        this.projectConfigurationForeWriterContractId = projectConfigurationForeWriterContractId;
+        this.divisionNumber = divisionNumber;
+        this.principalDivision = principalDivision;
+        this.lineOfBusiness = lineOfBusiness;
+        this.coverageType = coverageType;
+        this.currency = currency;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ProjectConfigurationForeWriterDivisionId")
---
+++ RR-Common/src/main/java/com/scor/rr/repository/ProjectConfigurationForeWriterDivisionRepository.java
@@ -0,0 +1,10 @@
+package com.scor.rr.repository;
+
+import com.scor.rr.domain.ProjectConfigurationForeWriterDivision;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by u004602 on 27/12/2019.
+ */
+public interface ProjectConfigurationForeWriterDivisionRepository extends JpaRepository<ProjectConfigurationForeWriterDivision, Long> {
+}
