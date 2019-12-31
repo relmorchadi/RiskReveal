802cf89e86724f5be230d4c1d7577cbbed05b472
NGUYEN Tien Khac (EXTERNAL) <TNGUYEN-EXTERNAL@scor.com>
2019-12-31 13:33:56 +0100

    API stats + fix bug find State by code name

--- RR-Common/src/main/java/com/scor/rr/repository/SummaryStatisticsDetailRepository.java
+++ RR-Common/src/main/java/com/scor/rr/repository/SummaryStatisticsDetailRepository.java
@@ -4,4 +4,5 @@ import com.scor.rr.domain.SummaryStatisticsDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SummaryStatisticsDetailRepository extends JpaRepository<SummaryStatisticsDetail, Long> {
+    SummaryStatisticsDetail findByPltHeaderIdAndCurveTypeAndLossType(Long pltId, String curveType, String lossType);
 }
