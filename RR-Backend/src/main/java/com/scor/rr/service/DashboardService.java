5a2b277c4f875159c7888b99d7dcd6e62b957afa
U009912 <OLAHLOU-EXTERNAL@scor.com>
2020-01-03 14:04:51 +0100

    Fix(?): fixed backEnd Api issue

--- RR-Backend/src/main/java/com/scor/rr/service/DashboardService.java
+++ RR-Backend/src/main/java/com/scor/rr/service/DashboardService.java
@@ -1,6 +1,5 @@
 package com.scor.rr.service;
 
-import com.scor.rr.domain.dto.DashBoardFilter;
 import com.scor.rr.domain.dto.DashboardRequest;
 import com.scor.rr.domain.dto.SortConfig;
 import com.scor.rr.domain.entities.DashboardView;
@@ -23,19 +22,22 @@ public class DashboardService {
     DashboardSpecification dashboardSpecification;
 
     public Page<DashboardView> getAll(DashboardRequest request) {
+        Sort s = null;
+        if (request.getSortConfig() != null && !request.getSortConfig().isEmpty()) {
+            SortConfig firstCol = request.getSortConfig().get(0);
 
-        SortConfig firstCol = request.getSortConfig().get(0);
-        Sort s= null;
-        if(!(firstCol == null)) {
-            s = new Sort(firstCol.getDirection().equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, firstCol.getField());
+            if (!(firstCol == null)) {
+                s = new Sort(firstCol.getDirection().equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, firstCol.getField());
 
-            for (int i = 0; i < request.getSortConfig().size(); i++) {
-                SortConfig tmp = request.getSortConfig().get(i);
-                s = s.and(new Sort(tmp.getDirection().equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, tmp.getField()));
+                for (int i = 0; i < request.getSortConfig().size(); i++) {
+                    SortConfig tmp = request.getSortConfig().get(i);
+                    s = s.and(new Sort(tmp.getDirection().equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, tmp.getField()));
+                }
             }
         }
 
-        return this.dashboardViewRepository.findAll(dashboardSpecification.getFilter(request.getFilterConfig()), PageRequest.of(request.getPageNumber(), request.getSize(), s));
+        return s == null ? this.dashboardViewRepository.findAll(dashboardSpecification.getFilter(request.getFilterConfig()), PageRequest.of(request.getPageNumber(), request.getSize())) :
+                this.dashboardViewRepository.findAll(dashboardSpecification.getFilter(request.getFilterConfig()), PageRequest.of(request.getPageNumber(), request.getSize(), s));
     }
 
 }
