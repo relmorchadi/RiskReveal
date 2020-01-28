87360011f1e4d533ae9e8303e1aac9236139daca
NGUYEN Tien Khac (EXTERNAL) <TNGUYEN-EXTERNAL@scor.com>
2020-01-28 15:47:38 +0100

    step load data

--- RR-Import/src/main/java/com/scor/rr/domain/RRFinancialPerspective.java
+++ RR-Import/src/main/java/com/scor/rr/domain/RRFinancialPerspective.java
@@ -12,7 +12,18 @@ public class RRFinancialPerspective {
     private long id;
     private String code;
     private String fullCode;
+    // FinancialPerspective
+    private String modellingVendor;
 
+    private String modellingSystem;
 
+    private String modellingSystemVersion;
+
+    public RRFinancialPerspective(String modellingVendor, String modellingSystem, String modellingSystemVersion, String code) {
+        this.modellingVendor = modellingVendor;
+        this.modellingSystem = modellingSystem;
+        this.modellingSystemVersion = modellingSystemVersion;
+        this.code = code;
+    }
     // TODO : review the rest with shaun and viet
 }
