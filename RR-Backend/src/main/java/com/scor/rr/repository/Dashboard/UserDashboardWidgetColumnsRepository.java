dcec6f1254c412ac490f354e45e09605f098b33d
U008761 <AKHARROU-EXTERNAL@scor.com>
2020-02-04 15:20:46 +0100

    90% complete

--- RR-Backend/src/main/java/com/scor/rr/repository/Dashboard/UserDashboardWidgetColumnsRepository.java
+++ RR-Backend/src/main/java/com/scor/rr/repository/Dashboard/UserDashboardWidgetColumnsRepository.java
@@ -2,6 +2,7 @@ package com.scor.rr.repository.Dashboard;
 
 import com.scor.rr.domain.entities.Dashboard.UserDashboardWidgetColumns;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,18 +19,22 @@ public interface UserDashboardWidgetColumnsRepository extends JpaRepository<User
     UserDashboardWidgetColumns findByUserDashboardWidgetColumnId(long id);
 
     @Transactional
+    @Modifying
     @Query(value = "exec dbonew.update_Widget_table_width @columnId=:columnId,  @width=:width", nativeQuery = true)
-    void updateDashboardWidgetColumnWidth(@Param("columnId") long columnId, @Param("width") Integer width);
+    void updateDashboardWidgetColumnWidth(@Param("columnId") long columnId, @Param("width") int width);
 
     @Transactional
+    @Modifying
     @Query(value = "exec dbonew.update_Widget_table_sort @columnId=:columnId,  @sort=:sort, @sorting=:sorting", nativeQuery = true)
-    void updateDashboardWidgetColumnSort(@Param("columnId") long columnId, @Param("sort") Integer sort, @Param("sorting") String sorting);
+    void updateDashboardWidgetColumnSort(@Param("columnId") long columnId, @Param("sort") int sort, @Param("sorting") String sorting);
 
     @Transactional
+    @Modifying
     @Query(value = "exec dbonew.update_Widget_table_filter @columnId=:columnId, @filter=:filter", nativeQuery = true)
     void updateDashboardWidgetColumnFilter(@Param("columnId") long columnId, @Param("filter") String filter);
 
     @Transactional
+    @Modifying
     @Query(value = "exec dbonew.update_Widget_table_visibleOrder @columnIdArray=:columnIdArray, @visibleArray=:visibleArray, @orderArray=:orderArray, @counter=:counter", nativeQuery = true)
     void updateDashboardWidgetColumnVisibilityAndOrder(@Param("columnIdArray") String columnIdArray, @Param("visibleArray") String isVisibleArray,@Param("orderArray") String orderArray,@Param("counter") int counter);
 
