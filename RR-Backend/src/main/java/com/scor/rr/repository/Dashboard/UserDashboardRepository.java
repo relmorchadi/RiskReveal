9ed9a4c4bd93d01442777be0b01d53b61f2e4071
U008761 <AKHARROU-EXTERNAL@scor.com>
2020-02-10 18:06:17 +0100

    StoredProcs for dashboard Tables complete

--- RR-Backend/src/main/java/com/scor/rr/repository/Dashboard/UserDashboardRepository.java
+++ RR-Backend/src/main/java/com/scor/rr/repository/Dashboard/UserDashboardRepository.java
@@ -1,10 +1,16 @@
 package com.scor.rr.repository.Dashboard;
 
 import com.scor.rr.domain.entities.Dashboard.UserDashboard;
+import com.scor.rr.domain.entities.DashboardView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface UserDashboardRepository extends JpaRepository<UserDashboard,Long> {
@@ -12,4 +18,26 @@ public interface UserDashboardRepository extends JpaRepository<UserDashboard,Lon
     List<UserDashboard> findByUserId(long userId);
     void deleteByUserDashboardId(long id);
     UserDashboard findByUserDashboardId(long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "exec dbonew.uspDashboardWidgetGetCarsBasedOnStatus @CarStatus=:carStatus," +
+            "  @Entity=:entity," +
+            " @UserDashboardWidgetId=:userDashboardWidgetId," +
+            "@UserCode=:userCode," +
+            "@PageNumber=:pageNumber," +
+            "@PageSize=:pageSize," +
+            "@SelectionList=:selectionList," +
+            "@SortSelectedFirst=:sortSelectedFirst," +
+            "@SortSelectedAction=:sortSelectedAction", nativeQuery = true)
+    List<Map<String,Object>> getDataForWidget(@Param("carStatus") String carStatus,
+                                              @Param("entity") int entity,
+                                              @Param("userDashboardWidgetId") long userDashboardWidgetId,
+                                              @Param("userCode") String userCode,
+                                              @Param("pageNumber") int pageNumber,
+                                              @Param("pageSize") int pageSize,
+                                              @Param("selectionList") String selectionList,
+                                              @Param("sortSelectedFirst") boolean sortSelectedFirst,
+                                              @Param("sortSelectedAction") String sortSelectedAction);
+
 }
