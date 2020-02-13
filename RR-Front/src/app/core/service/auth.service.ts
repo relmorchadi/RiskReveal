a0a60dd09b24a0fdb1b5b98a4ae7a94cc76a9c2a
U009912 <OLAHLOU-EXTERNAL@scor.com>
2020-02-13 10:45:37 +0100

    target-fix

--- RR-Front/src/app/core/service/auth.service.ts
+++ RR-Front/src/app/core/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 // import decode from 'jwt-decode';
 import {Select, Store} from "@ngxs/store";
+import * as _ from 'lodash';
 import {AuthState} from "../store/states/auth.state";
 
 @Injectable({
@@ -19,7 +20,8 @@ export class AuthService {
 
   public getToken() {
     this.token = window.localStorage.getItem('token');
-    return this.token;
+    return _.isNil(this.token) ? 'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE1ODE1ODY4NjQsInN1YiI6IlUwMDg5MjIiLCJleHAiOjE1ODIxOTE2NjR9.9khyqXFbgJkOJuxuqHNb686UOIbJBZhcoV0Jt_a-0xPUsgbEhtSFv8HpGiVGt40BNAMzU19Kw3yHMCNqQQOp4A' :
+        this.token;
   }
 
   public isAuthenticated(): boolean {
