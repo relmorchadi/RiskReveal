f467190b340180faf195662b7400dab024928585
EU\U008925 <U008925>
2020-02-19 15:33:09 +0100

    Imp(?): init user tabs

---
+++ RR-Backend/src/main/java/com/scor/rr/domain/entities/UserWorkspaceTabs.java
@@ -0,0 +1,14 @@
+package com.scor.rr.domain.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ZZ_UserWorkspaceTabs")
+@Data
+@NoArgsConstructor
+public class UserWorkspaceTabs {
+}
