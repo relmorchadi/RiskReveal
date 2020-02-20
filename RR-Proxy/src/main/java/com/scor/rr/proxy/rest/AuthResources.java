e8793b3d201864556b3f029e47d374d12297f92a
EU\U007632 <U007632>
2020-02-20 17:44:24 +0100

    Fix(?) Hide delete Edm/Rdm while scanning

--- RR-Proxy/src/main/java/com/scor/rr/proxy/rest/AuthResources.java
+++ RR-Proxy/src/main/java/com/scor/rr/proxy/rest/AuthResources.java
@@ -7,8 +7,6 @@ import com.scor.rr.proxy.dto.UserDto;
 import com.scor.rr.proxy.service.abstraction.HelperServiceAbs;
 import com.scor.rr.proxy.service.abstraction.UserServiceAbs;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.plugin.dom.exception.InvalidAccessException;
 
 @RestController
 @RequestMapping("/api")
@@ -42,15 +41,18 @@ public class AuthResources {
                 log.info("#11 value of username is ===> " + name);
                 String userId = helperService.getUserId();
                 UserDto userDto = new UserDto();
+                log.info("#11 value of userId is ===> " + userId);
 
-                userService.getUserByUserId(userId).ifPresent((UserRrEntity user) -> {
-                    userDto.setFullName(user.getFirstName() + " " + user.getLastName());
-                    userDto.setCode(user.getUserCode());
-                    userDto.setRole(user.getRole());
-                });
+                UserRrEntity user = userService.getUserByUserId(userId).orElseThrow(() -> new InvalidAccessException("error"));
+                userDto.setFullName(user.getFirstName() + " " + user.getLastName());
+                userDto.setCode(user.getUserCode());
+                userDto.setRole(user.getRole());
 
                 userDto.setJwtToken(jwtTokenProvider.generateToken(SecurityContextHolder.getContext().getAuthentication().getName().split("@")[0]));
                 return new ResponseEntity<>(userDto, HttpStatus.OK);
+            } catch (InvalidAccessException ex) {
+                log.error(ex.getMessage());
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
             } catch (Exception ex) {
                 log.error(ex.getMessage());
                 return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
