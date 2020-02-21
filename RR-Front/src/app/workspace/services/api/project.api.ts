af19103b821e5f2562311f0138f5b0abbc29afcf
U009912 <OLAHLOU-EXTERNAL@scor.com>
2020-02-21 10:22:38 +0100

    Fix(?): fix for UAT

--- RR-Front/src/app/workspace/services/api/project.api.ts
+++ RR-Front/src/app/workspace/services/api/project.api.ts
@@ -1,13 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {environment} from "../../../../environments/environment";
+import {backendUrl} from "../../../shared/api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectApi {
 
-  private readonly api = environment.API_URI + 'workspace/projects';
+  private readonly api = backendUrl() + 'workspace/projects';
 
   constructor(private http: HttpClient) {
   }
