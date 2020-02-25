c4e6f10778afe2070073582aecd528c51cb1911d
EU\U007632 <U007632>
2020-02-25 20:50:22 +0100

    Rework(?) Make user prefernce use the Security context user ID

--- RR-Backend/src/main/java/com/scor/rr/service/UserPreferenceService.java
+++ RR-Backend/src/main/java/com/scor/rr/service/UserPreferenceService.java
@@ -1,9 +1,12 @@
 package com.scor.rr.service;
 
 
+import com.scor.rr.configuration.security.UserPrincipal;
 import com.scor.rr.domain.UserPreference;
+import com.scor.rr.domain.UserRrEntity;
 import com.scor.rr.repository.UserPreferenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,17 +17,22 @@ public class UserPreferenceService {
     UserPreferenceRepository userPreferenceRepository;
 
     public UserPreference getUserPreferencesByUser(Long userId) {
-        return userPreferenceRepository.findByUserId(userId)
-                .orElseThrow(() -> new RuntimeException("No available user configuration for userId: "+ userId));
+        return userPreferenceRepository.findByUserId(getUserId())
+                .orElseThrow(() -> new RuntimeException("No available user configuration for userId: "+ getUserId()));
 
     }
 
     public UserPreference addUserPreferences(UserPreference userPreference) {
+        userPreference.setUserId(getUserId());
         return userPreferenceRepository.save(userPreference);
     }
 
     public void deleteUserPreference(Long userPreferenceId) {
         userPreferenceRepository.deleteById(userPreferenceId);
+    }
+
+    private Long getUserId(){
+        return ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser().getUserId();
     }
 }
 
