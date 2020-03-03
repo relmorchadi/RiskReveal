53a788c55979891a6e2b1341bd136938873c166b
EU\U007632 <U007632>
2020-03-03 14:56:07 +0100

    Draft(?)

--- RR-Import/src/main/java/com/scor/rr/service/batch/ELTToPLTConverter.java
+++ RR-Import/src/main/java/com/scor/rr/service/batch/ELTToPLTConverter.java
@@ -417,14 +417,6 @@ public class ELTToPLTConverter extends AbstractWriter {
                 }
             }
 
-            Date startConvert = new Date();
-            /** @TODO ...
-            for (PLTHeader scorPLTHeader : scorPLTHeaders) {
-            PLTConverterProgress pltConverterProgress = pltConverterProgressRepository.findByPltId(scorPLTHeader.getId());
-            pltConverterProgress.setStartConvert(startConvert);
-            pltConverterProgressRepository.save(pltConverterProgress);
-            }
-             */
             pool.execute(new TreatyBatchWorker(id, finished, queue, workerLatch, timer, scorPLTHeaderEntities, convertFunctionMapForPLT, outputStreamForPLT));
 
             FileChannel fc = null;
@@ -503,6 +495,9 @@ public class ELTToPLTConverter extends AbstractWriter {
             } finally {
                 log.info("Master {}: finish reading PEQT {}, total period count {}", this.id, peqtFile.getFileName(), periodCount);
                 IOUtils.closeQuietly(fc);
+                System.out.println("Used Memory before GC: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) );
+                System.gc();
+                System.out.println("Used Memory after  GC: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) );
                 pool.shutdown();
             }
 
@@ -517,7 +512,6 @@ public class ELTToPLTConverter extends AbstractWriter {
                 IOUtils.closeQuietly(entry.getValue());
             }
 
-            Date endConvert = new Date();
             for (PltHeaderEntity scorPltHeaderEntity : scorPLTHeaderEntities) {
                 /** @TODO Later ...
                 PLTConverterProgress pltConverterProgress = pltConverterProgressRepository.findByPltId(scorPLTHeader.getId());
@@ -627,8 +621,15 @@ public class ELTToPLTConverter extends AbstractWriter {
                         writeBufferToOutputStream(outputStream, pltLossDataList);
                     } catch (IOException e) {
                         log.error("Slave {}.{}: error {}", this.masterId, this.id, e);
+                    }finally {
+                        pltLossDataList.clear();
+                        try {
+                            if(outputStream != null)
+                                outputStream.close();
+                        }catch (IOException exp){
+                            exp.printStackTrace();
+                        }
                     }
-                    pltLossDataList.clear();
                 }
             }
             log.info("Slave {}.{}: finished}", this.masterId, this.id);
@@ -651,7 +652,7 @@ public class ELTToPLTConverter extends AbstractWriter {
             outputStream.write(out);
         }
 
-        private boolean process(RRPeriod p, Map<Long, ELTLossBetaConvertFunction> convertFunctionMap, double threshold, List<PLTLossData> pltLossDataList) throws IOException {
+        private boolean process(RRPeriod p, Map<Long, ELTLossBetaConvertFunction> convertFunctionMap, double threshold, List<PLTLossData> pltLossDataList) {
             int simPeriod = p.getNb();
             PLTLossPeriod lossPeriod = new PLTLossPeriod(simPeriod);
 
@@ -682,20 +683,6 @@ public class ELTToPLTConverter extends AbstractWriter {
                         convertFunctionMap.put(Long.valueOf(eventID), convertFunction);
                     }
                     eventLoss = convertFunction.getLossOverQuantile(quantile);
-                    if (eventID == 15067999 || eventID == 15068006 || eventID == 15068543 || eventID == 15067929 || eventID == 15049996 || eventID == 15038124 || eventID == 15218210 || eventID == 15031551 || eventID == 15038069) {
-                        System.out.println(simPeriod + ";" +
-                                eventID + ";" +
-                                convertFunction.getLoss() + ";" +
-                                convertFunction.getStdDevC() + ";" +
-                                convertFunction.getStdDevI() + ";" +
-                                convertFunction.getExposureValue() + ";" +
-                                convertFunction.getConvertFunction().getMu() + ";" +
-                                convertFunction.getConvertFunction().getSigma() + ";" +
-                                convertFunction.getConvertFunction().getAlpha() + ";" +
-                                convertFunction.getConvertFunction().getBeta() + ";" +
-                                quantile + ";" +
-                                eventLoss);
-                    }
                 }
                 if ((float) eventLoss > threshold) {
                     double maxExposure = convertFunction.getExposureValue();
