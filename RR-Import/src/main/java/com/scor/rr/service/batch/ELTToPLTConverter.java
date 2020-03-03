5ff558e0d816976278da75b106bce393ec2da1e2
EU\U007632 <U007632>
2020-03-03 15:43:05 +0100

    Fix(?) Optimize memory leaks

--- RR-Import/src/main/java/com/scor/rr/service/batch/ELTToPLTConverter.java
+++ RR-Import/src/main/java/com/scor/rr/service/batch/ELTToPLTConverter.java
@@ -627,8 +627,14 @@ public class ELTToPLTConverter extends AbstractWriter {
                         writeBufferToOutputStream(outputStream, pltLossDataList);
                     } catch (IOException e) {
                         log.error("Slave {}.{}: error {}", this.masterId, this.id, e);
+                    } finally {
+                        pltLossDataList.clear();
+                        try {
+                            outputStream.close();
+                        } catch (IOException exp) {
+                            exp.printStackTrace();
+                        }
                     }
-                    pltLossDataList.clear();
                 }
             }
             log.info("Slave {}.{}: finished}", this.masterId, this.id);
