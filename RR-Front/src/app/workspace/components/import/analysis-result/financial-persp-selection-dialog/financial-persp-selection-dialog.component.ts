b13b1c89bcfd25378b43ce776836f46832f02e0e
EU\U007632 <U007632>
2020-03-20 19:00:56 +0100

    Build prod fix

--- RR-Front/src/app/workspace/components/import/analysis-result/financial-persp-selection-dialog/financial-persp-selection-dialog.component.ts
+++ RR-Front/src/app/workspace/components/import/analysis-result/financial-persp-selection-dialog/financial-persp-selection-dialog.component.ts
@@ -242,13 +242,13 @@ export class FinancialPerspSelectionDialogComponent implements OnInit, OnChanges
     updateAllChecked(nextValue, type) {
         // console.log('check all', this.changes.analysis, nextValue);
         if (type == 'analysis') {
-            _.forEach(this.changes.analysis, (analysis, index) => this.changes.analysis[index] = {
+            _.forEach(this.changes.analysis, (analysis:any, index) => this.changes.analysis[index] = {
                 ...analysis,
                 selected: nextValue
             });
             this.updateTreeStateCheckBoxAnalysis();
         } else if (type == 'epc') {
-            _.forEach(this.changes.epCurves, (epc, index) => this.changes.epCurves[index] = {
+            _.forEach(this.changes.epCurves, (epc:any, index) => this.changes.epCurves[index] = {
                 ...epc,
                 selected: nextValue
             });
