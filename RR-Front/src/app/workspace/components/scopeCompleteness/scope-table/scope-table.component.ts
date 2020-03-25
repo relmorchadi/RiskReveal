da1e3ca875cb4f0809a54fca55a90ca542e7850a
U009912 <OLAHLOU-EXTERNAL@scor.com>
2020-03-25 18:05:59 +0100

    First scope of completeness delivery'

--- RR-Front/src/app/workspace/components/scopeCompleteness/scope-table/scope-table.component.ts
+++ RR-Front/src/app/workspace/components/scopeCompleteness/scope-table/scope-table.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Select, Store} from "@ngxs/store";
 import {WorkspaceState} from "../../../store/states";
 import {BaseContainer} from "../../../../shared/base";
@@ -11,10 +11,24 @@ import * as _ from "lodash";
   styleUrls: ['./scope-table.component.scss']
 })
 export class ScopeTableComponent extends BaseContainer implements OnInit {
+  @Output('attachPLTOpen') attachPltOpen: any = new EventEmitter<any>();
 
   @Input()
   dataSource;
+  @Input()
+  override;
+  @Input()
+  selectedSortBy;
+  @Input()
+  sortBy;
 
+  selectedDropDown: any;
+  regionInnerCodes: any = {};
+  targetInnerCodes: any = {};
+  workspaceType: any;
+  regionCodes: any = {};
+  targetCodes: any = {};
+  projectType: any;
   columns;
   keys;
 
@@ -22,6 +36,8 @@ export class ScopeTableComponent extends BaseContainer implements OnInit {
   selectedProject: any;
   @Select(WorkspaceState.getCurrentWS) currentWs$;
   currentWs;
+  @Select(WorkspaceState.getScopeCompletenessData)scopeData$;
+  scopeData;
 
   constructor( _baseStore: Store, _baseRouter: Router, _baseCdr: ChangeDetectorRef) {
     super(_baseRouter, _baseCdr, _baseStore);
@@ -30,57 +46,123 @@ export class ScopeTableComponent extends BaseContainer implements OnInit {
   ngOnInit() {
     this.selectedProject$.pipe().subscribe(value => {
       this.selectedProject = value;
+      this.projectType = _.get(value, 'projectType', 'FAC');
       this.detectChanges();
     });
     this.currentWs$.pipe().subscribe(value => {
       this.currentWs =  value;
+      this.workspaceType = _.get(value, 'workspaceType', 'fac');
       this.detectChanges();
-    })
+    });
+    this.scopeData$.pipe().subscribe(value => {
+      this.scopeData = value;
+      console.log(this.scopeData);
+      this.detectChanges();
+    });
+    this.initTableCols();
   }
 
   initTableCols() {
     this.columns = [
       {field: 'PLtHeader', type: 'multi', width: '420px'}
     ];
+    if (this.workspaceType === 'fac') {
+      if(this.projectType === 'FAC') {
+        _.forEach(this.selectedProject.divisions, item => {
+          this.columns = [...this.columns, {field: '', type: 'contractCol', width: '80px', topHeader: `Division N°${item}`}]
+        });
+      }
+    } else {
+      _.forEach(this.currentWs.treatySection, item => {
+        this.columns = [...this.columns, {field: '', type: 'contractCol', width: '80px', topHeader: item}]
+      });
+    }
+    this.columns = [...this.columns, {field: 'completion', header: 'Completion', type: 'commentary', width: '100px'}]
   }
 
   expandAllRows() {
-/*    const regions = [];
-    _.forEach(this.dataSource, data => {
-      this.keys[data.id] = true;
-      regions[data.id] = true;
-    });
-    if (_.keys(this.pTable.expandedRowKeys).length < _.keys(this.keys).length) {
-      this.pTable.expandedRowKeys = {...this.keys};
-      this.regionCodes = regions;
+    if(this.selectedSortBy == 'RAP / Minimum Grain') {
+      if (_.toArray(this.targetCodes).length < this.scopeData.targetRaps.length) {
+        _.forEach(this.scopeData.targetRaps, item => {
+          this.targetCodes[item.id] = true;
+        })
+      } else {
+        _.forEach(this.scopeData.targetRaps, item => {
+          _.forEach(item.regionPerils, rp => {
+            this.targetInnerCodes[rp.id + item.id] = true;
+          })
+        })
+      }
     } else {
-      _.forEach(this.dataSource, row => {
-        row.child.forEach(item => {
-          this.grains[item.id + row.id] = true;
-        });
-      });
-    }*/
-    // }this.grains[item.id + rowData.id]
-
+      if (_.toArray(this.regionCodes).length < this.scopeData.regionPerils.length) {
+        _.forEach(this.scopeData.regionPerils, item => {
+          this.regionCodes[item.id] = true;
+        })
+      } else {
+        _.forEach(this.scopeData.regionPerils, item => {
+          _.forEach(item.targetRaps, tr => {
+            this.regionInnerCodes[tr.id + item.id] = true;
+          })
+        })
+      }
+    }
   }
 
   closeAllRows() {
-/*    let check = false;
-    _.forEach(this.grains, item => {
-      if (item) {
-        check = true;
-      }
-    });
-    if (check) {
-      _.forEach(this.dataSource, row => {
-        row.child.forEach(item => {
-          this.grains[item.id + row.id] = false;
-        });
-      });
+    if (this.selectedSortBy == 'RAP / Minimum Grain') {
+      _.toArray(this.targetInnerCodes).length > 0 ? this.targetInnerCodes = {} : this.targetCodes = {};
     } else {
-      this.pTable.expandedRowKeys = {};
-      this.regionCodes = [];
-    }*/
+      _.toArray(this.regionInnerCodes).length > 0 ? this.regionInnerCodes = {} : this.regionCodes = {};
+    }
+  }
+
+  expandInnerRP(innerRowId, rowId) {
+    this.regionInnerCodes[innerRowId + rowId] = !this.regionInnerCodes[innerRowId + rowId];
+  }
+
+  expandInnerTR(innerRowId, rowId) {
+    this.targetInnerCodes[innerRowId + rowId] = !this.targetInnerCodes[innerRowId + rowId];
+  }
+
+  openAttachPLT() {
+    this.attachPltOpen.emit();
+  }
+
+  selectDropDown(event: boolean, rowId) {
+    if (event) {
+      this.selectedDropDown = rowId;
+    } else {
+      this.selectedDropDown = null;
+    }
+  }
+
+  checkRowAttached(row) {
+    let checked = true;
+    if (this.selectedSortBy == 'Minimum Grain / RAP') {
+      this.scopeData.regionPerils.forEach(rp => {
+            if (rp.id == row.id) {
+              if (!rp.attached && !rp.overridden) {
+                checked = false;
+              }
+            }
+          }
+      );
+    }
+    if (this.selectedSortBy == 'RAP / Minimum Grain') {
+      this.scopeData.targetRaps.forEach(tr => {
+            if (tr.id == row.id) {
+              if (!tr.attached && !tr.overridden) {
+                checked = false;
+              }
+            }
+          }
+      );
+    }
+    return checked;
+  }
+
+  overrideRow(row) {
+
   }
 
   selectedRow(row) {
--- RR-Front/src/app/workspace/services/api/scopeOfCompleteness.api.ts
+++ RR-Front/src/app/workspace/services/api/scopeOfCompleteness.api.ts
@@ -1,18 +1,38 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {backendUrl} from "../../../shared/api";
+import {backendUrl, importUrl} from "../../../shared/api";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ScopeOfCompletenessApi {
 
-    private readonly api = backendUrl();
+    private readonly api = `${backendUrl()}ScopeAndCompleteness/`;
 
-    constructor(private _http: HttpClient) {
+    constructor(private  http: HttpClient) {
     }
 
-    getData() {
-        return this._http.get(`${this.api}`, { params: {} });
+    getData(uwyear, workspaceId) {
+        return this.http.get(`${this.api}AccumulationPackage/getScopeOnly`, { params: {uwyear, workspaceId}});
+    }
+
+    attachePLTCreate(data) {
+        return this.http.post(`${this.api}AttachPLT/attachPLTs`, data, {responseType: 'text' as 'json'});
+    }
+
+    attachPLTDelete(pltIds) {
+        return this.http.delete(`${this.api}AttachPLT/attachPLTs`, { params: {pltIds}, responseType: 'text' as 'json'});
+    }
+
+    fileReader(fileName) {
+        return this.http.post(`${this.api}FWFileReader/create`, {},{ params: {fileName}, responseType: 'text' as 'json'});
+    }
+
+    overrideDone(data) {
+        return this.http.post(`${this.api}OverrideSection/override`, data, {responseType: 'text' as 'json'})
+    }
+
+    overrideDelete(overriddenSections) {
+        return this.http.delete(`${this.api}OverrideSection/override`, {params: {overriddenSections}, responseType: 'text' as 'json'})
     }
 }
