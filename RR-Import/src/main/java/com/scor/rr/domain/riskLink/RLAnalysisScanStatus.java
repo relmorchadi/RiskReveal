500241ebbc47f6c19a26b0fa3b8f07e93ebf4684
EU\U011139 <U011139>
2020-04-02 19:22:10 +0200

    Import basic scan optimization

--- RR-Import/src/main/java/com/scor/rr/domain/riskLink/RLAnalysisScanStatus.java
+++ RR-Import/src/main/java/com/scor/rr/domain/riskLink/RLAnalysisScanStatus.java
@@ -27,13 +27,13 @@ public class RLAnalysisScanStatus {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "LastScan")
     private Date lastScan;
-    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "RLModelAnalysisId")
-    private RLAnalysis rlAnalysis;
+   // @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
+  //  @JoinColumn(name = "RLModelAnalysisId")
+   // private RLAnalysis rlAnalysis;
 
     public RLAnalysisScanStatus(RLAnalysis rlAnalysisId, int scanStatus) {
         this.entity = 1;
-        this.rlAnalysis = rlAnalysisId;
+       // this.rlAnalysis = rlAnalysisId;
         this.scanLevel = 0; //TODO: define scan level enum
         this.scanStatus = scanStatus; //TODO: define scan status enum
         this.lastScan = new Date();
