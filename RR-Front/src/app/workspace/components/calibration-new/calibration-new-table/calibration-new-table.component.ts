629563475b59a9f64148c3a6b2b278194da0980e
AIT ELMKDEM Redouane (EXTERNAL) <RAITELMKDEM-EXTERNAL@scor.com>
2020-04-03 17:54:31 +0100

    Imp(?): table separator resize

--- RR-Front/src/app/workspace/components/calibration-new/calibration-new-table/calibration-new-table.component.ts
+++ RR-Front/src/app/workspace/components/calibration-new/calibration-new-table/calibration-new-table.component.ts
@@ -350,6 +350,7 @@ export class CalibrationNewTableComponent implements OnInit, AfterViewInit, Afte
   }
 
   onTableSeparatorResize(event) {
+    console.log('oe');
     const {currentWidthValue, originalWidthValue} = event;
     const delta = currentWidthValue - originalWidthValue;
 
