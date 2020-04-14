2911c5f50c91179550c41a14b58198436c979c4e
EU\U008925 <U008925>
2020-04-14 18:21:18 +0200

    Fix(?): number formatting for plt detail

--- RR-Front/src/app/shared/pipes/has-status.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HasStatusPipe } from './has-status.pipe';
-
-describe('HasStatusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HasStatusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
