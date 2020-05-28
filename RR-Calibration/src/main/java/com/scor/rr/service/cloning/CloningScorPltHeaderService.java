516ac4b04401c905ed64c957bd5c9c739f661bbb
AIT ELMKDEM Redouane (EXTERNAL) <RAITELMKDEM-EXTERNAL@scor.com>
2020-05-28 10:33:08 +0100

    fix display calib

--- RR-Calibration/src/main/java/com/scor/rr/service/cloning/CloningScorPltHeaderService.java
+++ RR-Calibration/src/main/java/com/scor/rr/service/cloning/CloningScorPltHeaderService.java
@@ -154,6 +154,9 @@ public class CloningScorPltHeaderService {
             AdjustmentThread newAdjustmentThread = new AdjustmentThread();
             newAdjustmentThread.setInitialPLT(newPurePlt);
             newAdjustmentThread.setFinalPLT(newPLT);
+            newAdjustmentThread.setLocked(false);
+            newAdjustmentThread.setThreadIndex(1);
+            newAdjustmentThread.setThreadStatus("valid");
             this.adjustmentThreadRepository.save(newAdjustmentThread);
             // clone model analysis
             Optional<ModelAnalysisEntity> modelAnalysisEntity = this.modelAnalysisEntityRepository.findById(sourcePlt.getModelAnalysisId());
@@ -173,13 +176,13 @@ public class CloningScorPltHeaderService {
             // copy plt files
             try {
                 File dstFile = this.copyPltFile(sourcePlt, newPLT ,
-                        "/scor/data/ihub/v4/Facultative/Contracts/"+ request.getTargetWorkspaceContextCode()
-                                + "/" + request.getTargetWorkspaceUwYear() + "/"  +
-                                this.projectRepository.findById(newPLT.getProjectId()).get().getProjectName()
+      //                  "/scor/data/ihub/v4/Facultative/Contracts/"+ request.getTargetWorkspaceContextCode()
+        //                        + "/" + request.getTargetWorkspaceUwYear() + "/"  +
+          //                      this.projectRepository.findById(newPLT.getProjectId()).get().getProjectName()
 
-//                        "C:\\dev\\projects\\test\\Facultative\\Contracts\\"  + request.getTargetWorkspaceContextCode()
-  //                              + "\\" + request.getTargetWorkspaceUwYear() + "\\"  +
-    //                            this.projectRepository.findById(newPLT.getProjectId()).get().getProjectName()
+                        "C:\\dev\\projects\\test\\Facultative\\Contracts\\"  + request.getTargetWorkspaceContextCode()
+                                + "\\" + request.getTargetWorkspaceUwYear() + "\\"  +
+                                this.projectRepository.findById(newPLT.getProjectId()).get().getProjectName()
                 );
                 newPLT.setLossDataFilePath(dstFile.getParent());
                 newPLT.setLossDataFileName(dstFile.getName());
